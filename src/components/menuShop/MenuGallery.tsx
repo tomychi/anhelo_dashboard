650d83636eec04007e18a5fcc20cd254a9d034ea
tomychi <tomychi352@gmail.com>
2023-12-20 15:46:46 -0300

    shop

--- src/components/menuShop/MenuGallery.tsx
+++ src/components/menuShop/MenuGallery.tsx
@@ -1,79 +1,72 @@
-import items from "../../assets/menu.json";
-import { CardItem } from "./CardItem";
+import { useEffect, useState } from 'react';
+import items from '../../assets/menu.json';
+import { CardItem } from './CardItem';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 interface Props {
-	handleFormBurger: (value: any) => void;
+  handleFormBurger: (value: any) => void;
 }
 
 export const MenuGallery = ({ handleFormBurger }: Props) => {
-	const originals = items.filter((item) => item.type === "originals");
-	const ourItems = items.filter((item) => item.type === "our");
-	const gaseosas = items.filter((item) => item.type === "gaseosas");
-	const papas = items.filter((item) => item.type === "papas");
+  const [data, setData] = useState([]);
+  const [initialLoadComplete, setInitialLoadComplete] = useState(false);
 
-	return (
-		<div className="flex flex-col">
-			<h1 className="text-custom-red font-antonio text-2xl font-black mb-4 ">
-				ORIGINALS
-			</h1>
-			<div className="grid grid-cols-6 md:grid-cols-6  gap-4 mb-4">
-				{/* Render all items side by side */}
-				{originals.map(({ name, img, id, price, type }) => (
-					<CardItem
-						key={id}
-						img={img}
-						name={name}
-						price={price}
-						type={type}
-						handleFormBurger={handleFormBurger}
-					/>
-				))}
-			</div>
-			<h1 className="text-custom-red font-antonio text-2xl mb-4 font-black">
-				MASTERPIECES
-			</h1>
-			<div className="grid grid-cols-6 md:grid-cols-6 gap-4 mb-4">
-				{ourItems.map(({ name, img, id, price, type }) => (
-					<CardItem
-						key={id}
-						img={img}
-						name={name}
-						price={price}
-						type={type}
-						handleFormBurger={handleFormBurger}
-					/>
-				))}
-			</div>
-			<h1 className="text-custom-red font-antonio text-2xl mb-4 font-black">
-				PAPAS
-			</h1>
-			<div className="grid grid-cols-6 md:grid-cols-6 gap-4 mb-4">
-				{papas.map(({ name, img, id, price, type }) => (
-					<CardItem
-						key={id}
-						img={img}
-						name={name}
-						price={price}
-						type={type}
-						handleFormBurger={handleFormBurger}
-					/>
-				))}
-			</div>
-			<h1 className="text-custom-red font-antonio text-2xl mb-4 font-black">
-				GASEOSAS
-			</h1>
-			<div className="grid grid-cols-6 md:grid-cols-6 gap-4">
-				{gaseosas.map(({ name, img, id, price, type }) => (
-					<CardItem
-						key={id}
-						img={img}
-						name={name}
-						price={price}
-						type={type}
-						handleFormBurger={handleFormBurger}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    // Función para cargar los datos iniciales desde Firestore
+    const fetchDataFromFirestore = async () => {
+      const firestore = getFirestore();
+
+      const collections = ['burgers', 'drinks', 'fries', 'toppings'];
+
+      const fetchedData = await Promise.all(
+        collections.map(async (collectionName) => {
+          const collectionRef = collection(firestore, collectionName);
+          const snapshot = await getDocs(collectionRef);
+
+          const dataWithIds = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+            collectionName: collectionName,
+          }));
+
+          return dataWithIds;
+        })
+      );
+
+      setData(fetchedData.flat());
+      setInitialLoadComplete(true); // Establecer la carga inicial como completa
+    };
+
+    // Solo cargamos datos si aún no se ha realizado la carga inicial
+    if (!initialLoadComplete) {
+      fetchDataFromFirestore();
+    }
+  }, [initialLoadComplete]);
+
+  return (
+    <div className="flex flex-col">
+      {['originals', 'masterpieces', 'papas', 'drink'].map((sectionName) => (
+        <div key={sectionName}>
+          <h1 className="text-custom-red font-antonio text-2xl font-black mb-4 ">
+            {sectionName.toUpperCase()}
+          </h1>
+          <div className="grid grid-cols-6 md:grid-cols-6 gap-4 mb-4">
+            {/* Renderizar items de la sección correspondiente */}
+            {data
+              .filter((item) => item.data.type === sectionName)
+              .map(({ id, data }) => (
+                <CardItem
+                  key={id}
+                  img={data.img}
+                  name={data.name}
+                  price={data.price}
+                  type={data.type}
+                  handleFormBurger={handleFormBurger}
+                />
+              ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/pages/DynamicForm.tsx
+++ src/pages/DynamicForm.tsx
@@ -1,214 +1,214 @@
-import { Formik, Form } from "formik";
+import { Formik, Form } from 'formik';
 
-import formJson from "../data/custom-form.json";
-import * as Yup from "yup";
-import { MySelect, MyTextInput } from "../components/forms";
-import { CartShop, MenuGallery } from "../components/menuShop";
-import { useState } from "react";
-import { PedidosWeb } from "../components/forms/PedidosWeb";
+import formJson from '../data/custom-form.json';
+import * as Yup from 'yup';
+import { MySelect, MyTextInput } from '../components/forms';
+import { CartShop, MenuGallery } from '../components/menuShop';
+import { useState } from 'react';
+import { PedidosWeb } from '../components/forms/PedidosWeb';
 
 const initialValues: { [key: string]: any } = {};
 const requiredFields: { [key: string]: any } = {};
 
 for (const input of formJson) {
-	initialValues[input.name] = input.value;
+  initialValues[input.name] = input.value;
 
-	if (!input.validations) continue;
+  if (!input.validations) continue;
 
-	let schema = Yup.string();
+  let schema = Yup.string();
 
-	for (const rule of input.validations) {
-		if (rule.type === "required") {
-			schema = schema.required(rule.message);
-		}
+  for (const rule of input.validations) {
+    if (rule.type === 'required') {
+      schema = schema.required(rule.message);
+    }
 
-		if (rule.type === "minLength") {
-			schema = schema.min((rule as any).value || 1, rule.message);
-		}
+    if (rule.type === 'minLength') {
+      schema = schema.min((rule as any).value || 1, rule.message);
+    }
 
-		if (rule.type === "email") {
-			schema = schema.email(rule.message);
-		}
+    if (rule.type === 'email') {
+      schema = schema.email(rule.message);
+    }
 
-		// otras reglas..
-	}
+    // otras reglas..
+  }
 
-	requiredFields[input.name] = schema;
+  requiredFields[input.name] = schema;
 }
 
 const validationSchema = Yup.object().shape({ ...requiredFields });
 
 interface ClienteProps {
-	aclaraciones: string;
-	direccion: string;
-	hora: string;
-	id: string;
-	metodoPago: string;
-	telefono: string;
-	total: number;
+  aclaraciones: string;
+  direccion: string;
+  hora: string;
+  id: string;
+  metodoPago: string;
+  telefono: string;
+  total: number;
 }
 
 export interface BurgerProps {
-	burger: string;
-	quantity: number;
-	toppings: string[];
-	priceBurger: number;
-	priceToppings: number;
-	subTotal: number;
+  burger: string;
+  quantity: number;
+  toppings: string[];
+  priceBurger: number;
+  priceToppings: number;
+  subTotal: number;
 }
 
 export const DynamicForm = () => {
-	const [dataBurger, setDataBurger] = useState({
-		burgerSelection: [] as BurgerProps[],
-	});
+  const [dataBurger, setDataBurger] = useState({
+    burgerSelection: [] as BurgerProps[],
+  });
 
-	const [seccionActiva, setSeccionActiva] = useState("elaborar");
+  const [seccionActiva, setSeccionActiva] = useState('elaborar');
 
-	// si es el formulario de la seccion burgers
-	const handleFormBurger = (values: any) => {
-		const burger = {
-			burger: values.burger,
-			toppings: values.toppings,
-			quantity: values.quantity,
-			priceBurger: values.priceBurger,
-			priceToppings: values.priceToppings,
-			subTotal: (values.priceBurger + values.priceToppings) * values.quantity,
-		};
+  // si es el formulario de la seccion burgers
+  const handleFormBurger = (values: any) => {
+    const burger = {
+      burger: values.burger,
+      toppings: values.toppings,
+      quantity: values.quantity,
+      priceBurger: values.priceBurger,
+      priceToppings: values.priceToppings,
+      subTotal: (values.priceBurger + values.priceToppings) * values.quantity,
+    };
 
-		setDataBurger((prevData) => ({
-			...prevData,
-			burgerSelection: [...prevData.burgerSelection, burger],
-		}));
-	};
+    setDataBurger((prevData) => ({
+      ...prevData,
+      burgerSelection: [...prevData.burgerSelection, burger],
+    }));
+  };
 
-	const handlePedidoWebAnalizado = (detallesPedido: any) => {
-		// Aquí puedes agregar lógica adicional según sea necesario
+  const handlePedidoWebAnalizado = (detallesPedido: any) => {
+    // Aquí puedes agregar lógica adicional según sea necesario
 
-		console.log(detallesPedido);
-	};
-	console.log(dataBurger);
+    console.log(detallesPedido);
+  };
+  console.log(dataBurger);
 
-	return (
-		<div className="p-4 w-3/5">
-			<div>
-				{dataBurger && (
-					<CartShop burgerSelection={dataBurger.burgerSelection} />
-				)}
-				<MenuGallery handleFormBurger={handleFormBurger} />
-			</div>
+  return (
+    <div className="p-4 w-3/5">
+      <div>
+        {dataBurger && (
+          <CartShop burgerSelection={dataBurger.burgerSelection} />
+        )}
+        <MenuGallery handleFormBurger={handleFormBurger} />
+      </div>
 
-			<div>
-				<Formik
-					initialValues={initialValues}
-					validationSchema={validationSchema}
-					onSubmit={(values, { resetForm }) => {
-						const subTotal = dataBurger.burgerSelection.reduce(
-							(acc, burger) => acc + burger.subTotal,
-							0
-						);
+      <div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values, { resetForm }) => {
+            const subTotal = dataBurger.burgerSelection.reduce(
+              (acc, burger) => acc + burger.subTotal,
+              0
+            );
 
-						const orderDetail = {
-							...values,
-							subTotal,
-							total: subTotal + values.envio,
-							burgerSelection: dataBurger.burgerSelection,
-						};
+            const orderDetail = {
+              ...values,
+              subTotal,
+              total: subTotal + values.envio,
+              burgerSelection: dataBurger.burgerSelection,
+            };
 
-						console.log(orderDetail);
+            console.log(orderDetail);
 
-						setDataBurger({ burgerSelection: [] });
+            setDataBurger({ burgerSelection: [] });
 
-						resetForm();
-					}}
-				>
-					{(formik) => (
-						<Form
-							noValidate
-							className="grid fixed right-6 top-4 z-0 w-1/3 font-antonio font-black ml-2 bg-custom-red"
-						>
-							<div className="flex flex-col">
-								<div className="flex justify-center my-2">
-									<div
-										className={`mx-2 py-2 px-4 ${
-											seccionActiva === "elaborar"
-												? "bg-black text-custom-red"
-												: "bg-custom-red text-black"
-										} text-black  `}
-										onClick={() => setSeccionActiva("elaborar")}
-									>
-										TOMAR PEDIDO
-									</div>
-									<div
-										className={`mx-2 py-2 px-4 ${
-											seccionActiva === "elaborar"
-												? "bg-custom-red text-black"
-												: "bg-black text-custom-red"
-										} font-semibold `}
-										onClick={() => setSeccionActiva("hechos")}
-									>
-										HECHOS POR LA WEB
-									</div>
-								</div>
-								{seccionActiva === "elaborar" ? (
-									formJson.map(
-										({ type, name, placeholder, label, options }) => {
-											if (
-												type === "text" ||
-												type === "tel" ||
-												type === "number"
-											) {
-												return (
-													<div className="flex flex-col items-center justify-center">
-														<MyTextInput
-															key={name}
-															type={type as any}
-															name={name}
-															label={label}
-														/>
-													</div>
-												);
-											} else if (type === "select") {
-												return (
-													<MySelect
-														key={name}
-														label={label}
-														name={name}
-														style={{
-															backgroundColor: "black",
-															color: "#FE0000",
-														}}
-													>
-														{options?.map(({ id, label }) => (
-															<option
-																className="h-16 hover:bg-red-700"
-																key={id}
-																value={id}
-																defaultValue={"efectivo"}
-															>
-																{label}
-															</option>
-														))}
-													</MySelect>
-												);
-											}
+            resetForm();
+          }}
+        >
+          {(formik) => (
+            <Form
+              noValidate
+              className="grid fixed right-6 top-4 z-0 w-1/3 font-antonio font-black ml-2 bg-custom-red"
+            >
+              <div className="flex flex-col">
+                <div className="flex justify-center my-2">
+                  <div
+                    className={`mx-2 py-2 px-4 ${
+                      seccionActiva === 'elaborar'
+                        ? 'bg-black text-custom-red'
+                        : 'bg-custom-red text-black'
+                    } text-black  `}
+                    onClick={() => setSeccionActiva('elaborar')}
+                  >
+                    TOMAR PEDIDO
+                  </div>
+                  <div
+                    className={`mx-2 py-2 px-4 ${
+                      seccionActiva === 'elaborar'
+                        ? 'bg-custom-red text-black'
+                        : 'bg-black text-custom-red'
+                    } font-semibold `}
+                    onClick={() => setSeccionActiva('hechos')}
+                  >
+                    HECHOS POR LA WEB
+                  </div>
+                </div>
+                {seccionActiva === 'elaborar' ? (
+                  formJson.map(
+                    ({ type, name, placeholder, label, options }) => {
+                      if (
+                        type === 'text' ||
+                        type === 'tel' ||
+                        type === 'number'
+                      ) {
+                        return (
+                          <div className="flex flex-col items-center justify-center">
+                            <MyTextInput
+                              key={name}
+                              type={type as any}
+                              name={name}
+                              label={label}
+                            />
+                          </div>
+                        );
+                      } else if (type === 'select') {
+                        return (
+                          <MySelect
+                            key={name}
+                            label={label}
+                            name={name}
+                            style={{
+                              backgroundColor: 'black',
+                              color: '#FE0000',
+                            }}
+                          >
+                            {options?.map(({ id, label }) => (
+                              <option
+                                className="h-16 hover:bg-red-700"
+                                key={id}
+                                value={id}
+                                defaultValue={'efectivo'}
+                              >
+                                {label}
+                              </option>
+                            ))}
+                          </MySelect>
+                        );
+                      }
 
-											throw new Error(`Invalid type: ${type}`);
-										}
-									)
-								) : (
-									<PedidosWeb onPedidoAnalizado={handlePedidoWebAnalizado} />
-								)}
-							</div>
-							<button
-								className="  text-custom-red p-4 bg-black font-black uppercase text-4x1 outline-none "
-								type="submit"
-							>
-								Guardar
-							</button>
-						</Form>
-					)}
-				</Formik>
-			</div>
-		</div>
-	);
+                      throw new Error(`Invalid type: ${type}`);
+                    }
+                  )
+                ) : (
+                  <PedidosWeb onPedidoAnalizado={handlePedidoWebAnalizado} />
+                )}
+              </div>
+              <button
+                className="  text-custom-red p-4 bg-black font-black uppercase text-4x1 outline-none "
+                type="submit"
+              >
+                Guardar
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 };
