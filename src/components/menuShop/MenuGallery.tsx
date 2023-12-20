8467f9320f61215c8cdf6582cfb774d60d0b48d7
Tobías <tobiasazcurrabi@gmail.com>
2023-12-20 12:14:47 -0300

    wqfq

--- src/components/menuShop/MenuGallery.tsx
+++ src/components/menuShop/MenuGallery.tsx
@@ -8,6 +8,8 @@ interface Props {
 export const MenuGallery = ({ handleFormBurger }: Props) => {
 	const originals = items.filter((item) => item.type === "originals");
 	const ourItems = items.filter((item) => item.type === "our");
+	const gaseosas = items.filter((item) => item.type === "gaseosas");
+	const papas = items.filter((item) => item.type === "papas");
 
 	return (
 		<div className="flex flex-col">
@@ -30,8 +32,38 @@ export const MenuGallery = ({ handleFormBurger }: Props) => {
 			<h1 className="text-custom-red font-antonio text-2xl mb-4 font-black">
 				MASTERPIECES
 			</h1>
-			<div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+			<div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-4">
 				{ourItems.map(({ name, img, id, price, type }) => (
+					<CardItem
+						key={id}
+						img={img}
+						name={name}
+						price={price}
+						type={type}
+						handleFormBurger={handleFormBurger}
+					/>
+				))}
+			</div>
+			<h1 className="text-custom-red font-antonio text-2xl mb-4 font-black">
+				PAPAS
+			</h1>
+			<div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-4">
+				{papas.map(({ name, img, id, price, type }) => (
+					<CardItem
+						key={id}
+						img={img}
+						name={name}
+						price={price}
+						type={type}
+						handleFormBurger={handleFormBurger}
+					/>
+				))}
+			</div>
+			<h1 className="text-custom-red font-antonio text-2xl mb-4 font-black">
+				GASEOSAS
+			</h1>
+			<div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+				{gaseosas.map(({ name, img, id, price, type }) => (
 					<CardItem
 						key={id}
 						img={img}
