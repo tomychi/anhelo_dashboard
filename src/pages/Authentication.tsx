4ebef38874ffc096daecffe320cc7cff11025b1b
Tobías <tobiasazcurrabi@gmail.com>
2024-02-04 20:23:45 -0300

    asfasfas

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -1,9 +1,10 @@
-import { Login } from '../components/auth/Login';
+import { Login } from "../components/auth/Login";
 
 export const Authentication = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+	return (
+		//El que lee es gay
+		<div className=" bg-custom-red w-full h-full flex items-center ">
+			<Login />
+		</div>
+	);
 };
