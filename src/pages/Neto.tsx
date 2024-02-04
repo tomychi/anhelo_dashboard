c644c75827186fd3d1064b9a8e55620242a3b3fe
Tobías <tobiasazcurrabi@gmail.com>
2024-02-04 19:33:24 -0300

    ergeg

--- src/pages/Neto.tsx
+++ src/pages/Neto.tsx
@@ -127,9 +127,35 @@ export const Neto = () => {
 		],
 	};
 
+	const materialesInfo = {
+		"Bolsa kraft + Sticker": 169.1,
+		Aluminio: 100.0,
+		"Sticker ADVISORY": 40.0,
+		"Bolsita papas": 10.0,
+		"Papas 130 gr": 507.0,
+		"Bollo de pan": 230.0,
+		"Medallon 90gr": 225.02,
+		"Cheddar feta": 78.6,
+		Mayonesa: 24.6,
+		"Bacon feta x2": 199.36,
+		Huevo: 100.0,
+		Alioli: 30.0,
+		BBQ: 30.0,
+		Ketchup: 30.0,
+		"Crema de leche": 100.0,
+		Cebolla: 70.0,
+		Harina: 10.0,
+		Champis: 200.0,
+		Lechuga: 70.0,
+		"Tomate feta": 70.0,
+		"Pote cheddar": 46.46,
+		"Cheddar liquido": 233.33,
+		Caja: 357.0,
+	};
+
 	return (
 		<div className="flex p-4 gap-4 justify-between flex-row">
-			<table className="w-1/2 font-antonio text-sm text-left rtl:text-right text-black">
+			<table className="w-4/5 font-antonio text-sm text-left rtl:text-right text-black">
 				<thead className="text-xs  uppercase text-black border border-red-main bg-custom-red ">
 					<tr>
 						<th scope="col" className="px-6 py-3">
@@ -176,73 +202,34 @@ export const Neto = () => {
 					)}
 				</tbody>
 			</table>
-			<table className="w-1/2 font-antonio text-sm text-left rtl:text-right text-black">
+			<table className="w-1/5 font-antonio text-sm text-left rtl:text-right text-black">
 				<thead className="text-xs  uppercase text-black border border-red-main bg-custom-red ">
 					{/* Encabezados de la tabla */}
 					<tr>
 						<th scope="col" className="px-6 py-3">
-							Product name
+							materiales
 						</th>
 						<th scope="col" className="px-6 py-3">
-							Category
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Fecha
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Descripcion
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Cantidad
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Unidad
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Total
-						</th>
-						<th scope="col" className="px-6 py-3">
-							<span className="sr-only">Edit</span>
+							precio
 						</th>
 					</tr>
 				</thead>
 				<tbody>
-					{/* Mapeo de datos de burgers */}
-					{expenseData.map(
-						({
-							quantity,
-							fecha,
-							category,
-							name,
-							total,
-							unit,
-							description,
-							id,
-						}) => (
-							<tr
-								key={id}
-								className="bg-black text-custom-red uppercase font-black border border-red-main"
+					{/* Mapeo de datos de materiales */}
+					{Object.entries(materialesInfo).map(([material, precio], index) => (
+						<tr
+							key={index}
+							className="bg-black text-custom-red uppercase font-black border border-red-main"
+						>
+							<th
+								scope="row"
+								className="px-6 py-4 font-black text-custom-red whitespace-nowrap"
 							>
-								<th
-									scope="row"
-									className="px-6 py-4 font-black text-custom-red whitespace-nowrap "
-								>
-									{name}
-								</th>
-								<td className="px-6 py-4">{category}</td>
-								<td className="px-6 py-4">{fecha}</td>
-								<td className="px-6 py-4 ">{description}</td>
-								<td className="px-6 py-4 ">{quantity}</td>
-								<td className="px-6 py-4 ">{unit}</td>
-								<td className="px-6 py-4">{currencyFormat(total)}</td>
-								<td className="px-6 py-4 text-center">
-									<div className="font-black border border-red-main text-custom-red hover:underline">
-										Borrar
-									</div>
-								</td>
-							</tr>
-						)
-					)}
+								{material}
+							</th>
+							<td className="px-6 py-4">{currencyFormat(precio)}</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 		</div>
