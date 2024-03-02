8555b725380e57ad2715d81381785dfa6966206b
Tobías <tobiasazcurrabi@gmail.com>
2024-03-02 11:37:02 -0300

    asdgasd

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -1,180 +1,180 @@
-import { useEffect, useState } from 'react';
-import { Card } from '../components/Card';
-import { ReadOrdersForToday } from '../firebase/ReadData';
-import { PedidoProps } from '../types/types';
-import { RootState } from '../redux/configureStore';
-import { useSelector, useDispatch } from 'react-redux';
-import { readOrdersData } from '../redux/data/dataAction';
-import { useLocation } from 'react-router-dom';
-import currencyFormat from '../helpers/currencyFormat';
-import { calcularPromedioTiempoElaboracion } from '../helpers/dateToday';
+import { useEffect, useState } from "react";
+import { Card } from "../components/Card";
+import { ReadOrdersForToday } from "../firebase/ReadData";
+import { PedidoProps } from "../types/types";
+import { RootState } from "../redux/configureStore";
+import { useSelector, useDispatch } from "react-redux";
+import { readOrdersData } from "../redux/data/dataAction";
+import { useLocation } from "react-router-dom";
+import currencyFormat from "../helpers/currencyFormat";
+import { calcularPromedioTiempoElaboracion } from "../helpers/dateToday";
 
 export const Comandera = () => {
-  const [seccionActiva, setSeccionActiva] = useState('porHacer');
-  const [cadeteSeleccionado, setCadeteSeleccionado] = useState('');
-  const [sumaTotalPedidos, setSumaTotalPedidos] = useState(0);
-  const [sumaTotalEfectivo, setSumaTotalEfectivo] = useState(0);
-  const [promedioTiempoElaboracion, setPromedioTiempoElaboracion] = useState(0);
+	const [seccionActiva, setSeccionActiva] = useState("porHacer");
+	const [cadeteSeleccionado, setCadeteSeleccionado] = useState("");
+	const [sumaTotalPedidos, setSumaTotalPedidos] = useState(0);
+	const [sumaTotalEfectivo, setSumaTotalEfectivo] = useState(0);
+	const [promedioTiempoElaboracion, setPromedioTiempoElaboracion] = useState(0);
 
-  const disptach = useDispatch();
-  const { orders } = useSelector((state: RootState) => state.data);
-  const location = useLocation();
-  // Filtra los pedidos según la sección activa
-  const pedidosPorHacer = orders.filter(
-    (o) =>
-      !o.elaborado && (!cadeteSeleccionado || o.cadete === cadeteSeleccionado)
-  );
-  const pedidosHechos = orders.filter(
-    (o) =>
-      o.elaborado && (!cadeteSeleccionado || o.cadete === cadeteSeleccionado)
-  );
-  useEffect(() => {
-    const promedio = calcularPromedioTiempoElaboracion(pedidosHechos);
+	const disptach = useDispatch();
+	const { orders } = useSelector((state: RootState) => state.data);
+	const location = useLocation();
+	// Filtra los pedidos según la sección activa
+	const pedidosPorHacer = orders.filter(
+		(o) =>
+			!o.elaborado && (!cadeteSeleccionado || o.cadete === cadeteSeleccionado)
+	);
+	const pedidosHechos = orders.filter(
+		(o) =>
+			o.elaborado && (!cadeteSeleccionado || o.cadete === cadeteSeleccionado)
+	);
+	useEffect(() => {
+		const promedio = calcularPromedioTiempoElaboracion(pedidosHechos);
 
-    setPromedioTiempoElaboracion(promedio);
-    if (location.pathname === '/comandas') {
-      const unsubscribe = ReadOrdersForToday((pedidos: PedidoProps[]) => {
-        disptach(readOrdersData(pedidos));
-      });
+		setPromedioTiempoElaboracion(promedio);
+		if (location.pathname === "/comandas") {
+			const unsubscribe = ReadOrdersForToday((pedidos: PedidoProps[]) => {
+				disptach(readOrdersData(pedidos));
+			});
 
-      return () => {
-        unsubscribe(); // Detiene la suscripción cuando el componente se desmonta
-      };
-    }
-  }, [disptach, location, pedidosHechos]);
+			return () => {
+				unsubscribe(); // Detiene la suscripción cuando el componente se desmonta
+			};
+		}
+	}, [disptach, location, pedidosHechos]);
 
-  const cadetesUnicos = Array.from(
-    new Set(orders.map((order) => order.cadete))
-  );
-  // Manejar el cambio en el select de cadetes
-  const handleCadeteChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const nuevoCadeteSeleccionado = event.target.value;
-    setCadeteSeleccionado(nuevoCadeteSeleccionado);
+	const cadetesUnicos = Array.from(
+		new Set(orders.map((order) => order.cadete))
+	);
+	// Manejar el cambio en el select de cadetes
+	const handleCadeteChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const nuevoCadeteSeleccionado = event.target.value;
+		setCadeteSeleccionado(nuevoCadeteSeleccionado);
 
-    // Calcular la suma total de pedidos para el cadete seleccionado
-    const totalPedidosCadete = orders.reduce((total, pedido) => {
-      if (
-        cadeteSeleccionado === '' ||
-        pedido.cadete === nuevoCadeteSeleccionado
-      ) {
-        return total + 1; // Si no se ha seleccionado ningún cadete o si el cadete del pedido coincide con el seleccionado, sumar 1 al total
-      } else {
-        return total;
-      }
-    }, 0);
+		// Calcular la suma total de pedidos para el cadete seleccionado
+		const totalPedidosCadete = orders.reduce((total, pedido) => {
+			if (
+				cadeteSeleccionado === "" ||
+				pedido.cadete === nuevoCadeteSeleccionado
+			) {
+				return total + 1; // Si no se ha seleccionado ningún cadete o si el cadete del pedido coincide con el seleccionado, sumar 1 al total
+			} else {
+				return total;
+			}
+		}, 0);
 
-    setSumaTotalPedidos(totalPedidosCadete);
+		setSumaTotalPedidos(totalPedidosCadete);
 
-    // Calcular la suma total de los montos de los pedidos que fueron en efectivo para el cadete seleccionado
-    const totalEfectivoCadete = orders.reduce((total, pedido) => {
-      if (
-        (cadeteSeleccionado === '' ||
-          pedido.cadete === nuevoCadeteSeleccionado) &&
-        pedido.metodoPago === 'efectivo'
-      ) {
-        return total + pedido.total; // Si no se ha seleccionado ningún cadete o si el cadete del pedido coincide con el seleccionado y el pago fue en efectivo, sumar el monto total del pedido
-      } else {
-        return total;
-      }
-    }, 0);
+		// Calcular la suma total de los montos de los pedidos que fueron en efectivo para el cadete seleccionado
+		const totalEfectivoCadete = orders.reduce((total, pedido) => {
+			if (
+				(cadeteSeleccionado === "" ||
+					pedido.cadete === nuevoCadeteSeleccionado) &&
+				pedido.metodoPago === "efectivo"
+			) {
+				return total + pedido.total; // Si no se ha seleccionado ningún cadete o si el cadete del pedido coincide con el seleccionado y el pago fue en efectivo, sumar el monto total del pedido
+			} else {
+				return total;
+			}
+		}, 0);
 
-    setSumaTotalEfectivo(totalEfectivoCadete);
-  };
+		setSumaTotalEfectivo(totalEfectivoCadete);
+	};
 
-  const customerSuccess =
-    100 -
-    (orders.filter((order) => order.dislike || order.delay).length * 100) /
-      orders.length;
-  return (
-    <div>
-      <div className="flex justify-center font-antonio my-4">
-        <button
-          className={`mx-2 py-2 px-4 ${
-            seccionActiva === 'porHacer'
-              ? 'bg-custom-red'
-              : 'border-2 border-red-main text-custom-red'
-          } text-black font-black uppercase `}
-          onClick={() => setSeccionActiva('porHacer')}
-        >
-          Por Hacer
-        </button>
-        <button
-          className={`mx-2 py-2 px-4 ${
-            seccionActiva === 'hechos'
-              ? 'bg-custom-red'
-              : 'border-2 border-red-main text-custom-red'
-          } text-black font-black uppercase `}
-          onClick={() => setSeccionActiva('hechos')}
-        >
-          Hechos
-        </button>
-      </div>
-      <div className="text-custom-red uppercase font-antonio font-black   flex flex-col gap-4 items-center">
-        <p className="border-b-2 border-red-main ">
-          Customer success: {customerSuccess}%
-        </p>
+	const customerSuccess =
+		100 -
+		(orders.filter((order) => order.dislike || order.delay).length * 100) /
+			orders.length;
+	return (
+		<div>
+			<div className="flex justify-center font-antonio my-4">
+				<button
+					className={`mx-2 py-2 px-4 ${
+						seccionActiva === "porHacer"
+							? "bg-custom-red"
+							: "border-2 border-red-main text-custom-red"
+					} text-black font-black uppercase `}
+					onClick={() => setSeccionActiva("porHacer")}
+				>
+					Por Hacer
+				</button>
+				<button
+					className={`mx-2 py-2 px-4 ${
+						seccionActiva === "hechos"
+							? "bg-custom-red"
+							: "border-2 border-red-main text-custom-red"
+					} text-black font-black uppercase `}
+					onClick={() => setSeccionActiva("hechos")}
+				>
+					Hechos
+				</button>
+			</div>
+			<div className="text-custom-red uppercase font-antonio   flex flex-col gap-4 items-center">
+				<p className="border-b-2 font-black border-red-main ">
+					Customer success: {customerSuccess}%
+				</p>
 
-        <p>
-          Promedio de tiempo de elaboración por pedido:{' '}
-          {promedioTiempoElaboracion} minutos
-        </p>
+				<p className=" border-b-2 font-black border-red-main">
+					Promedio de tiempo de elaboración por pedido:{" "}
+					{promedioTiempoElaboracion} minutos
+				</p>
 
-        <div className="flex flex-row border-b-2 border-red-main">
-          <p>Filtrar por cadetes:</p>
-          <select
-            value={cadeteSeleccionado}
-            onChange={handleCadeteChange}
-            className="bg-black uppercase"
-          >
-            <option value="">Todos los cadetes</option>
-            {cadetesUnicos.map((cadete, index) => {
-              if (cadete === undefined) return;
-              return (
-                <option key={index} value={cadete}>
-                  {cadete}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        {cadeteSeleccionado && (
-          <div>
-            <p>
-              Suma total de pedidos para {cadeteSeleccionado}:{' '}
-              {sumaTotalPedidos}
-            </p>
-            <p>
-              Suma total de pagos en efectivo para {cadeteSeleccionado}:{' '}
-              {currencyFormat(sumaTotalEfectivo)}
-            </p>
-          </div>
-        )}
-      </div>
-      <div className="grid grid-cols-2 p-4 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
-        {seccionActiva === 'porHacer' ? (
-          Array.isArray(pedidosPorHacer) && pedidosPorHacer.length > 0 ? (
-            pedidosPorHacer.map((comanda) => (
-              <div key={comanda.id}>
-                <Card comanda={comanda} />
-              </div>
-            ))
-          ) : (
-            <p className="text-custom-red font-antonio p-4">
-              No hay pedidos por hacer.
-            </p>
-          )
-        ) : Array.isArray(pedidosHechos) && pedidosHechos.length > 0 ? (
-          pedidosHechos.map((comanda) => (
-            <div key={comanda.id}>
-              <Card comanda={comanda} />
-            </div>
-          ))
-        ) : (
-          <p className="text-custom-red font-antonio p-4">
-            No hay pedidos hechos.
-          </p>
-        )}
-      </div>
-    </div>
-  );
+				<div className="flex flex-row border-b-2 font-black border-red-main">
+					<p>Filtrar por cadetes:</p>
+					<select
+						value={cadeteSeleccionado}
+						onChange={handleCadeteChange}
+						className="bg-black uppercase"
+					>
+						<option value="">Todos los cadetes</option>
+						{cadetesUnicos.map((cadete, index) => {
+							if (cadete === undefined) return;
+							return (
+								<option key={index} value={cadete}>
+									{cadete}
+								</option>
+							);
+						})}
+					</select>
+				</div>
+				{cadeteSeleccionado && (
+					<div>
+						<p>
+							Suma total de pedidos para {cadeteSeleccionado}:{" "}
+							{sumaTotalPedidos}
+						</p>
+						<p>
+							Suma total de pagos en efectivo para {cadeteSeleccionado}:{" "}
+							{currencyFormat(sumaTotalEfectivo)}
+						</p>
+					</div>
+				)}
+			</div>
+			<div className="grid grid-cols-2 p-4 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+				{seccionActiva === "porHacer" ? (
+					Array.isArray(pedidosPorHacer) && pedidosPorHacer.length > 0 ? (
+						pedidosPorHacer.map((comanda) => (
+							<div key={comanda.id}>
+								<Card comanda={comanda} />
+							</div>
+						))
+					) : (
+						<p className="text-custom-red font-antonio p-4">
+							No hay pedidos por hacer.
+						</p>
+					)
+				) : Array.isArray(pedidosHechos) && pedidosHechos.length > 0 ? (
+					pedidosHechos.map((comanda) => (
+						<div key={comanda.id}>
+							<Card comanda={comanda} />
+						</div>
+					))
+				) : (
+					<p className="text-custom-red font-antonio p-4">
+						No hay pedidos hechos.
+					</p>
+				)}
+			</div>
+		</div>
+	);
 };
