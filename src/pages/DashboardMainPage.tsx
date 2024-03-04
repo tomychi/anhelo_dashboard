9952175fa9ae77f027624ca4300a0880d238654b
Tobías <tobiasazcurrabi@gmail.com>
2024-03-04 11:48:19 -0300

    hsdfhs

--- src/pages/DashboardMainPage.tsx
+++ src/pages/DashboardMainPage.tsx
@@ -1,40 +1,42 @@
-import { Routes, Route } from 'react-router-dom';
-import { Sidebar } from '../components/sidebar';
+import { Routes, Route } from "react-router-dom";
+import { Sidebar } from "../components/sidebar";
 import {
-  DynamicForm,
-  Comandera,
-  Delivery,
-  Settings,
-  Dashboard,
-  Gastos,
-  Neto,
-  Bruto,
-  Seguidores,
-  ProductosVendidos,
-} from '../pages';
+	DynamicForm,
+	Comandera,
+	Delivery,
+	Settings,
+	Dashboard,
+	Gastos,
+	Neto,
+	Bruto,
+	Seguidores,
+	ProductosVendidos,
+} from "../pages";
+import { MonthData } from "./MonthData";
 
 export const DashboardMainPage = () => {
-  return (
-    <div className="flex h-screen bg-black overflow-x-hidden">
-      <Sidebar />
-      <div className="flex-grow overflow-y-auto">
-        <Routes>
-          {/* Agrega un comodín '*' a la ruta principal */}
-          <Route path="/*">
-            <Route index element={<Dashboard />} />
-            <Route path="pedidos" element={<DynamicForm />} />
-            <Route path="comandas" element={<Comandera />} />
-            <Route path="delivery" element={<Delivery />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="gastos" element={<Gastos />} />
-            <Route path="neto" element={<Neto />} />
-            <Route path="bruto" element={<Bruto />} />
-            <Route path="seguidores" element={<Seguidores />} />
-            <Route path="productosVendidos" element={<ProductosVendidos />} />
-            <Route path="ventas" element={<Comandera />} />
-          </Route>
-        </Routes>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex h-screen bg-black overflow-x-hidden">
+			<Sidebar />
+			<div className="flex-grow overflow-y-auto">
+				<Routes>
+					{/* Agrega un comodín '*' a la ruta principal */}
+					<Route path="/*">
+						<Route index element={<Dashboard />} />
+						<Route path="pedidos" element={<DynamicForm />} />
+						<Route path="comandas" element={<Comandera />} />
+						<Route path="delivery" element={<Delivery />} />
+						<Route path="settings" element={<Settings />} />
+						<Route path="gastos" element={<Gastos />} />
+						<Route path="neto" element={<Neto />} />
+						<Route path="bruto" element={<Bruto />} />
+						<Route path="seguidores" element={<Seguidores />} />
+						<Route path="productosVendidos" element={<ProductosVendidos />} />
+						<Route path="ventas" element={<Comandera />} />
+						<Route path="monthdata" element={<MonthData />} />
+					</Route>
+				</Routes>
+			</div>
+		</div>
+	);
 };
