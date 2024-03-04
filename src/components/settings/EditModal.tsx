61e111ac7cb369bc3ab3d4ef8306e55f25e1afeb
Tobías <tobiasazcurrabi@gmail.com>
2024-03-04 12:37:12 -0300

    adsg

--- src/components/settings/EditModal.tsx
+++ src/components/settings/EditModal.tsx
@@ -81,68 +81,74 @@ export const EditModal = ({
 					</div>
 				</div>
 				<div className="uppercase font-antonio font-black">
-					<div className="grid gap-4 mb-4 grid-cols-2">
-						<div className="col-span-2">
-							<label className="block mb-2 text-sm text-black">Name</label>
+					<div className="flex flex-col gap-4 ">
+						<div className="relative z-0 mt-3">
+							<label className="peer-focus:font-medium uppercase absolute text-sm texk-black 500 texk-black 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+								Name
+							</label>
 							<input
 								type="text"
 								name="name"
 								id="name"
 								value={formData.name}
 								onChange={handleInputChange}
-								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+								className="block py-2.5 texk-black w-full  bg-transparent border-0 border-b-2 border-black appearance-none text-black focus:outline-none focus:ring-0 peer uppercase"
 								placeholder="Type product name"
 							/>
 						</div>
-						<div className="col-span-2">
-							<label className="block mb-2 text-sm text-black">Image</label>
+						<div className="relative z-0 mt-4  ">
+							<label className="peer-focus:font-medium uppercase absolute text-sm texk-black 500 texk-black 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+								Image
+							</label>
 							<input
 								type="text"
 								name="name"
 								id="name"
 								value={formData.img}
 								onChange={handleInputChange}
-								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+								className="block py-2.5 texk-black w-full  bg-transparent border-0 border-b-2 border-black appearance-none text-black focus:outline-none focus:ring-0 peer uppercase"
 								placeholder="Image"
 							/>
 						</div>
-						<div className="col-span-2 sm:col-span-1">
-							<label className="block mb-2 text-sm text-black">Price</label>
+						<div className="relative z-0 mt-4  ">
+							<label className="peer-focus:font-medium uppercase absolute text-sm texk-black 500 texk-black 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+								Price
+							</label>
 							<input
 								type="number"
 								name="price"
 								id="price"
 								value={formData.price}
 								onChange={handleInputChange}
-								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+								className="block py-2.5 texk-black w-full  bg-transparent border-0 border-b-2 border-black appearance-none text-black focus:outline-none focus:ring-0 peer uppercase"
 								placeholder="$2999"
 							/>
 						</div>
 
-						<div className="col-span-2 sm:col-span-1">
-							<label className="block mb-2 text-sm text-black">Type</label>
+						<div className="relative z-0 mt-4  ">
+							<label className="peer-focus:font-medium uppercase absolute text-sm texk-black 500 texk-black 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+								Type
+							</label>
 							<input
 								type="text"
 								name="type"
 								id="type"
 								value={formData.type}
 								onChange={handleInputChange}
-								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+								className="block py-2.5 texk-black w-full  bg-transparent border-0 border-b-2 border-black appearance-none text-black focus:outline-none focus:ring-0 peer uppercase"
 								placeholder="$2999"
 							/>
 						</div>
-						<div className="col-span-2">
-							<label className="block mb-2 text-sm text-black">
+						<div className="relative z-0 mt-4  ">
+							<label className="peer-focus:font-medium uppercase absolute text-sm texk-black 500 texk-black 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 text-black peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
 								Product Description
 							</label>
-							<textarea
+							<input
 								id="description"
-								rows={4}
 								value={formData.description}
 								onChange={handleInputChange}
-								className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-								placeholder="Write product description here"
-							></textarea>
+								className="block py-2.5 texk-black w-full  bg-transparent border-0 border-b-2 border-black uppercase appearance-none text-black focus:outline-none focus:ring-0 peer"
+							></input>
 						</div>
 					</div>
 					<ConfirmationModal handleConfirmation={handleConfirmation} />
