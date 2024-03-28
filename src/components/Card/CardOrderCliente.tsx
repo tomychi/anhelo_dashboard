b176b38d43edba60f64a70fde93fb8ba1d5065b0
Tobías <tobiasazcurrabi@gmail.com>
2024-03-28 13:24:38 -0300

    asdgasd

--- src/components/Card/CardOrderCliente.tsx
+++ src/components/Card/CardOrderCliente.tsx
@@ -1,75 +1,75 @@
-import React from 'react';
-import { PedidoProps } from '../../types/types';
+import React from "react";
+import { PedidoProps } from "../../types/types";
 
 interface CardOrderCliente {
-  p: PedidoProps;
+	p: PedidoProps;
 }
 
 export const CardOrderCliente = ({ p }: CardOrderCliente) => {
-  return (
-    <div key={p.id} className="border rounded-md p-4 mb-4 bg-gray-100">
-      <h3 className="text-lg font-semibold mb-2">Detalles del Pedido</h3>
-      <p>
-        <span className="font-semibold">ID del Pedido:</span> {p.id}
-      </p>
-      <p>
-        <span className="font-semibold">Dirección:</span> {p.direccion}
-      </p>
-      <p>
-        <span className="font-semibold">Aclaraciones:</span> {p.aclaraciones}
-      </p>
-      <p>
-        <span className="font-semibold">Fecha:</span> {p.fecha}
-      </p>
+	return (
+		<div key={p.id} className="border rounded-md p-4 mb-4 bg-gray-100">
+			<h3 className="text-lg font-semibold mb-2">Detalles del Pedido</h3>
+			<p>
+				<span className="font-semibold">ID del Pedido:</span> {p.id}
+			</p>
+			<p>
+				<span className="font-semibold">Dirección:</span> {p.direccion}
+			</p>
+			<p>
+				<span className="font-semibold">Aclaraciones:</span> {p.aclaraciones}
+			</p>
+			<p>
+				<span className="font-semibold">Fecha:</span> {p.fecha}
+			</p>
 
-      <p>
-        <span className="font-semibold">Método de Pago:</span> {p.metodoPago}
-      </p>
-      <p>
-        <span className="font-semibold">Subtotal:</span> ${p.subTotal}
-      </p>
-      <p>
-        <span className="font-semibold">Envío:</span> ${p.envio}
-      </p>
-      <p>
-        <span className="font-semibold">Total:</span> ${p.total}
-      </p>
-      <p>
-        <span className="font-semibold">Referencias:</span> {p.referencias}
-      </p>
-      <p>
-        <span className="font-semibold">Piso:</span> {p.piso}
-      </p>
-      {p.cadete && (
-        <p>
-          <span className="font-semibold">Cadete:</span> {p.cadete}
-        </p>
-      )}
-      {p.dislike && (
-        <p>
-          <span className="font-semibold">Dislike:</span> {p.dislike}
-        </p>
-      )}
-      {p.delay && (
-        <p>
-          <span className="font-semibold">Delay:</span> {p.delay}
-        </p>
-      )}
-      <p>
-        <span className="font-semibold">Hora:</span> {p.hora}
-      </p>
-      <p>
-        <span className="font-semibold">Tiempo Elaborado:</span>{' '}
-        {p.tiempoElaborado}
-      </p>
-      <p>
-        <span className="font-semibold">Tiempo Entregado:</span>{' '}
-        {p.tiempoEntregado}
-      </p>
-      <p>
-        <span className="font-semibold">¿Entregado?:</span>{' '}
-        {p.entregado ? 'Sí' : 'No'}
-      </p>
-    </div>
-  );
+			<p>
+				<span className="font-semibold">Método de Pago:</span> {p.metodoPago}
+			</p>
+			<p>
+				<span className="font-semibold">Subtotal:</span> ${p.subTotal}
+			</p>
+			<p>
+				<span className="font-semibold">Envío:</span> ${p.envio}
+			</p>
+			<p>
+				<span className="font-semibold">Total:</span> ${p.total}
+			</p>
+			<p>
+				<span className="font-semibold">Referencias:</span> {p.referencias}
+			</p>
+			<p>
+				<span className="font-semibold">Piso:</span> {p.piso}
+			</p>
+			{p.cadete && (
+				<p>
+					<span className="font-semibold">Cadete:</span> {p.cadete}
+				</p>
+			)}
+			{p.dislike && (
+				<p>
+					<span className="font-semibold">Dislike:</span> {p.dislike}
+				</p>
+			)}
+			{p.delay && (
+				<p>
+					<span className="font-semibold">Delay:</span> {p.delay}
+				</p>
+			)}
+			<p>
+				<span className="font-semibold">Hora:</span> {p.hora}
+			</p>
+			<p>
+				<span className="font-semibold">Tiempo Elaborado:</span>{" "}
+				{p.tiempoElaborado}
+			</p>
+			<p>
+				<span className="font-semibold">Tiempo Entregado:</span>{" "}
+				{p.tiempoEntregado}
+			</p>
+			<p>
+				<span className="font-semibold">¿Entregado?:</span>{" "}
+				{p.entregado ? "Sí" : "No"}
+			</p>
+		</div>
+	);
 };
--- src/components/Card/TiempoEditable.tsx
+++ src/components/Card/TiempoEditable.tsx
@@ -1,70 +1,91 @@
-import { useState } from 'react';
-import { RootState } from '../../redux/configureStore';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { RootState } from "../../redux/configureStore";
+import { useSelector } from "react-redux";
 
 export interface TiempoEditableProps {
-  title: string;
-  tiempoInicial: string;
-  pedidoId: string;
-  fecha: string;
-  updateTiempoForOrder: (
-    fechaPedido: string,
-    pedidoId: string,
-    nuevoTiempo: string
-  ) => Promise<void>;
+	title: string;
+	tiempoInicial: string;
+	pedidoId: string;
+	fecha: string;
+	updateTiempoForOrder: (
+		fechaPedido: string,
+		pedidoId: string,
+		nuevoTiempo: string
+	) => Promise<void>;
 }
 
 export const TiempoEditable = ({
-  title,
-  tiempoInicial,
-  pedidoId,
-  fecha,
-  updateTiempoForOrder,
+	title,
+	tiempoInicial,
+	pedidoId,
+	fecha,
+	updateTiempoForOrder,
 }: TiempoEditableProps) => {
-  // Estado para almacenar el tiempo actual y el nuevo tiempo que se está editando
-  const [tiempo, setTiempo] = useState(tiempoInicial);
-  const [nuevoTiempo, setNuevoTiempo] = useState(tiempoInicial);
-  const user = useSelector((state: RootState) => state.auth.user);
+	// Estado para almacenar el tiempo actual y el nuevo tiempo que se está editando
+	const [tiempo, setTiempo] = useState(tiempoInicial);
+	const [nuevoTiempo, setNuevoTiempo] = useState(tiempoInicial);
+	const [editandoTiempo, setEditandoTiempo] = useState(false);
+	const user = useSelector((state: RootState) => state.auth.user);
 
-  // Función para manejar el cambio en el nuevo tiempo
-  const handleNuevoTiempoChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setNuevoTiempo(event.target.value);
-  };
+	// Función para manejar el cambio en el nuevo tiempo
+	const handleNuevoTiempoChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setNuevoTiempo(event.target.value);
+	};
 
-  // Función para manejar la actualización del tiempo
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+	// Función para manejar el envío del formulario de edición del tiempo
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
 
-    // Llama a la función para actualizar el tiempo
-    updateTiempoForOrder(fecha, pedidoId, nuevoTiempo)
-      .then(() => {
-        // Si la actualización es exitosa, actualiza el estado del tiempo actual
-        setTiempo(nuevoTiempo);
-        // Limpia el estado del nuevo tiempo
-        setNuevoTiempo('');
-      })
-      .catch((error) => {
-        // Maneja cualquier error que ocurra durante la actualización del tiempo
-        console.error('Error actualizando el tiempo:', error);
-      });
-  };
+		// Llama a la función para actualizar el tiempo
+		updateTiempoForOrder(fecha, pedidoId, nuevoTiempo)
+			.then(() => {
+				// Si la actualización es exitosa, actualiza el estado del tiempo actual
+				setTiempo(nuevoTiempo);
+				// Limpia el estado del nuevo tiempo
+				setNuevoTiempo("");
+				// Establece el estado de edición en falso para ocultar el formulario
+				setEditandoTiempo(false);
+			})
+			.catch((error) => {
+				// Maneja cualquier error que ocurra durante la actualización del tiempo
+				console.error("Error actualizando el tiempo:", error);
+			});
+	};
 
-  return (
-    <div>
-      {/* Muestra el tiempo actual y un formulario para editarlo */}
-      <p>{`${title}: ${tiempo}`}</p>
-      {user.email === 'cadetes@anhelo.com' ? null : (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={nuevoTiempo}
-            onChange={handleNuevoTiempoChange}
-          />
-          <button type="submit">Guardar</button>
-        </form>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<div className="flex flex-row  justify-between">
+				{/* Muestra el tiempo actual y un formulario para editarlo */}
+				<p className="text-xs border-2 p-1 border-white text-white w-full">{`${title}: ${tiempo} HS`}</p>
+				{/* Renderiza el botón de edición si el usuario no es un cadete */}
+				{!editandoTiempo && user.email !== "cadetes@anhelo.com" && (
+					<button
+						onClick={() => setEditandoTiempo(true)}
+						className="text-xs border-2 text-white uppercase p-1"
+					>
+						Editar
+					</button>
+				)}
+			</div>
+			{/* Renderiza el formulario de edición del tiempo si se está editando */}
+			{editandoTiempo && (
+				<form onSubmit={handleSubmit} className="w-full flex flex-row">
+					<input
+						type="text"
+						value={nuevoTiempo}
+						onChange={handleNuevoTiempoChange}
+						className="bg-white uppercase text-xs font-black w-full  text-green-500 p-1 border-2 border-white"
+					/>
+					<button
+						type="submit"
+						className="uppercase border-2 border-white text-xs text-white p-1"
+					>
+						Guardar
+					</button>
+				</form>
+			)}
+		</div>
+	);
 };
