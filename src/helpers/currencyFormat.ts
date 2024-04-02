5d40cedb6f6a213c6e878cea6369e4e1a93bbc71
Tobías <tobiasazcurrabi@gmail.com>
2024-04-02 12:13:20 -0300

    asdga

--- src/helpers/currencyFormat.ts
+++ src/helpers/currencyFormat.ts
@@ -1,14 +1,14 @@
-export default function currencyFormat(num: number) {
-  if (num === null) {
-    return '0'; // O cualquier otro valor predeterminado que desees devolver si el valor es nulo
-  }
-  const aux = num.toFixed(2);
-  let result = '$' + aux.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+export default function currencyFormat(num: number | undefined | null) {
+	if (num === undefined || num === null) {
+		return "0"; // O cualquier otro valor predeterminado que desees devolver si el valor es nulo o indefinido
+	}
+	const aux = num.toFixed(2);
+	let result = "$" + aux.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
 
-  // Verificar si los últimos dos caracteres son "00" y eliminarlos si es necesario
-  if (result.endsWith('00')) {
-    result = result.slice(0, -3); // Eliminar los últimos dos caracteres
-  }
+	// Verificar si los últimos dos caracteres son "00" y eliminarlos si es necesario
+	if (result.endsWith("00")) {
+		result = result.slice(0, -3); // Eliminar los últimos dos caracteres
+	}
 
-  return result;
+	return result;
 }
