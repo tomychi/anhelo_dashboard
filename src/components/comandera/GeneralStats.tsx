078ed5878ef28a176b5aaa46a742b542589f8712
Tobías <tobiasazcurrabi@gmail.com>
2024-04-23 22:19:38 -0300

    gdas

--- src/components/comandera/GeneralStats.tsx
+++ src/components/comandera/GeneralStats.tsx
@@ -26,35 +26,38 @@ export const GeneralStats = ({
 }: GeneralStatsProps) => {
 	return (
 		<div className="text-custom-red uppercase font-antonio flex flex-col gap-4 mb-8">
-			<div className="flex w-max  flex-row border-2 pl-1.5 font-black border-red-main">
-				<p>Filtrar por cadetes:</p>
-				<select
-					value={cadeteSeleccionado}
-					onChange={handleCadeteChange}
-					className="bg-black uppercase"
-				>
-					<option value="">Todos los cadetes</option>
-					{cadetesUnicos.map((cadete: string, index: number) => {
-						if (cadete === undefined) return;
-						return (
-							<option key={index} value={cadete}>
-								{cadete}
-							</option>
-						);
-					})}
-				</select>
-			</div>
-			{cadeteSeleccionado && (
-				<div>
-					<p>
-						Suma total de pedidos para {cadeteSeleccionado}: {sumaTotalPedidos}
-					</p>
-					<p>
-						Suma total de pagos en efectivo para {cadeteSeleccionado}:{" "}
-						{currencyFormat(sumaTotalEfectivo)}
-					</p>
+			<div className="flex flex-col gap-2">
+				<div className="flex w-max  flex-row border-2 pl-1.5 font-black border-red-main">
+					<p>Filtrar por cadetes:</p>
+					<select
+						value={cadeteSeleccionado}
+						onChange={handleCadeteChange}
+						className="bg-black uppercase"
+					>
+						<option value="">Todos los cadetes</option>
+						{cadetesUnicos.map((cadete: string, index: number) => {
+							if (cadete === undefined) return;
+							return (
+								<option key={index} value={cadete}>
+									{cadete}
+								</option>
+							);
+						})}
+					</select>
 				</div>
-			)}
+				{cadeteSeleccionado && (
+					<div>
+						<p>
+							Suma total de pedidos para {cadeteSeleccionado}:{" "}
+							{sumaTotalPedidos}
+						</p>
+						<p>
+							Suma total de pagos en efectivo para {cadeteSeleccionado}:{" "}
+							{currencyFormat(sumaTotalEfectivo)}
+						</p>
+					</div>
+				)}
+			</div>
 			<div>
 				<p className="border-b-2 font-black w-max border-red-main">
 					PEDIDOS ENTREGADOS A TIEMPO: {Math.round(customerSuccess)}%
