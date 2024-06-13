dd6912fa58f5ebdb39f6dd44520ec825a734bfb0
tomy <tomychi352@gmail.com>
2024-06-13 11:06:11 -0300

    mapa vueltas cadetes

--- src/components/comandera/NavButtons.tsx
+++ src/components/comandera/NavButtons.tsx
@@ -1,68 +1,68 @@
 interface NavButtonsProps {
-	seccionActiva: string;
-	setSeccionActiva: (seccion: string) => void;
+  seccionActiva: string;
+  setSeccionActiva: (seccion: string) => void;
 }
 
 export const NavButtons = ({
-	seccionActiva,
-	setSeccionActiva,
+  seccionActiva,
+  setSeccionActiva,
 }: NavButtonsProps) => {
-	return (
-		<div className="">
-			<div className="flex font-antonio flex-row gap-2 mb-4">
-				<button
-					className={`p-2 ${
-						seccionActiva === "porHacer"
-							? "bg-custom-red"
-							: "border-2 border-red-main text-custom-red"
-					} text-black font-black uppercase `}
-					onClick={() => setSeccionActiva("porHacer")}
-				>
-					Hacer
-				</button>
-				<button
-					className={`p-2 ${
-						seccionActiva === "hechos"
-							? "bg-custom-red"
-							: "border-2 border-red-main text-custom-red"
-					} text-black font-black uppercase `}
-					onClick={() => setSeccionActiva("hechos")}
-				>
-					Hechos
-				</button>
-				<button
-					className={`p-2 ${
-						seccionActiva === "entregados"
-							? "bg-custom-red"
-							: "border-2 border-red-main text-custom-red"
-					} text-black font-black uppercase `}
-					onClick={() => setSeccionActiva("entregados")}
-				>
-					Entregados
-				</button>
-				{/* <button
-					className={`p-2 ${
-						seccionActiva === "mapa"
-							? "bg-custom-red"
-							: "border-2 border-red-main text-custom-red"
-					} text-black font-black uppercase `}
-					onClick={() => setSeccionActiva("mapa")}
-				>
-					Mapa
-				</button> */}
+  return (
+    <div className="">
+      <div className="flex font-antonio flex-row gap-2 mb-4">
+        <button
+          className={`p-2 ${
+            seccionActiva === 'porHacer'
+              ? 'bg-custom-red'
+              : 'border-2 border-red-main text-custom-red'
+          } text-black font-black uppercase `}
+          onClick={() => setSeccionActiva('porHacer')}
+        >
+          Hacer
+        </button>
+        <button
+          className={`p-2 ${
+            seccionActiva === 'hechos'
+              ? 'bg-custom-red'
+              : 'border-2 border-red-main text-custom-red'
+          } text-black font-black uppercase `}
+          onClick={() => setSeccionActiva('hechos')}
+        >
+          Hechos
+        </button>
+        <button
+          className={`p-2 ${
+            seccionActiva === 'entregados'
+              ? 'bg-custom-red'
+              : 'border-2 border-red-main text-custom-red'
+          } text-black font-black uppercase `}
+          onClick={() => setSeccionActiva('entregados')}
+        >
+          Entregados
+        </button>
+        <button
+          className={`p-2 ${
+            seccionActiva === 'mapa'
+              ? 'bg-custom-red'
+              : 'border-2 border-red-main text-custom-red'
+          } text-black font-black uppercase `}
+          onClick={() => setSeccionActiva('mapa')}
+        >
+          Mapa
+        </button>
 
-				{/* boton para ver las vueltas */}
-				<button
-					className={`p-2 ${
-						seccionActiva === "vueltas"
-							? "bg-custom-red"
-							: "border-2 border-red-main text-custom-red"
-					} text-black font-black uppercase `}
-					onClick={() => setSeccionActiva("vueltas")}
-				>
-					Vueltas
-				</button>
-			</div>
-		</div>
-	);
+        {/* boton para ver las vueltas */}
+        <button
+          className={`p-2 ${
+            seccionActiva === 'vueltas'
+              ? 'bg-custom-red'
+              : 'border-2 border-red-main text-custom-red'
+          } text-black font-black uppercase `}
+          onClick={() => setSeccionActiva('vueltas')}
+        >
+          Vueltas
+        </button>
+      </div>
+    </div>
+  );
 };
