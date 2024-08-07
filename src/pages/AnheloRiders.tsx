aa8e517676235690d168ff149319b184dd885b2c
Tobías <tobiasazcurrabi@gmail.com>
2024-08-07 19:12:09 -0300

    gsdgs

--- src/pages/AnheloRiders.tsx
+++ src/pages/AnheloRiders.tsx
@@ -61,12 +61,42 @@ export const AnheloRiders = () => {
 						</p>
 					</div>
 					<div className="flex flex-col justify-between">
-						<p className="uppercase p-2 flex items-center h-1/2 text-center justify-center bg-green-500 font-black text-black font-antonio">
-							LLAMAR
-						</p>
-						<p className="uppercase p-2 flex items-center h-1/2 bg-green-500 font-black justify-center text-black font-antonio">
-							ENTREGAR
-						</p>
+						<div className="uppercase p-2 flex flex-row items-center h-1/2 bg-white font-black justify-center text-black font-antonio gap-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								className=" h-4"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+								/>
+							</svg>
+
+							<p>LLAMAR</p>
+						</div>
+						<div className="uppercase p-2 flex flex-row items-center h-1/2 bg-green-500 font-black justify-center text-black font-antonio gap-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								className=" h-5"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+								/>
+							</svg>
+
+							<p>ENTREGAR</p>
+						</div>
 					</div>
 				</div>
 				<div className="flex flex-row  justify-between border  border-red-main">
