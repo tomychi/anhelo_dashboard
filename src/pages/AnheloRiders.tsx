b8c507397628fabe6bf6ccd04345892706a334bc
Tobías <tobiasazcurrabi@gmail.com>
2024-08-07 17:53:32 -0300

    asdf

---
+++ src/pages/AnheloRiders.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import mapa from "../assets/mapa.png";
+
+export const AnheloRiders = () => {
+	return (
+		<div className="flex flex-col">
+			{/* Aca la parte de la plata */}
+			<div className="bg-red-main flex flex-row  justify-between p-4">
+				<div className="">
+					<div className="flex flex-row gap-2 items-baseline">
+						<p className="text-4xl font-black font-antonio">$7020</p>
+						<p className="text-xs font-black font-antonio uppercase">
+							ganancia
+						</p>
+					</div>
+					<div className="flex flex-row gap-2 items-baseline">
+						<p className="text-4xl font-black font-antonio">$280</p>
+						<p className="text-xs font-black font-antonio uppercase">propina</p>
+					</div>
+				</div>
+				<div className="flex flex-col items-end ">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						className="w-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+						/>
+					</svg>
+					<p className="text-xs font-black font-antonio uppercase">
+						ver detalles
+					</p>
+				</div>
+			</div>
+			{/* Aca la parte del mapa */}
+			<div>
+				<img src={mapa} alt="mapa" />
+			</div>
+			{/* Aca la parte de los pedidos */}
+			<div></div>
+		</div>
+	);
+};
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,18 +1,20 @@
-export { Clientes } from './Clientes';
+export { Clientes } from "./Clientes";
 
-export { Gastos } from './Gastos';
-export { Neto } from './Neto';
-export { Bruto } from './Bruto';
-export { Seguidores } from './Seguidores';
-export { ProductosVendidos } from './ProductosVendidos';
+export { Gastos } from "./Gastos";
+export { Neto } from "./Neto";
+export { Bruto } from "./Bruto";
+export { Seguidores } from "./Seguidores";
+export { ProductosVendidos } from "./ProductosVendidos";
 
-export { Error } from './Error';
+export { Error } from "./Error";
 
-export { Authentication } from './Authentication';
-export { Settings } from './Settings';
+export { Authentication } from "./Authentication";
+export { Settings } from "./Settings";
 
-export { Dashboard } from './Dashboard';
+export { Dashboard } from "./Dashboard";
 
-export { Comandera } from './Comandera';
-export { Delivery } from './Delivery';
-export { DynamicForm } from './DynamicForm';
+export { Comandera } from "./Comandera";
+export { Delivery } from "./Delivery";
+export { DynamicForm } from "./DynamicForm";
+
+export { AnheloRiders } from "./AnheloRiders";
