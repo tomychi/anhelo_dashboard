d524e6fe3c31c1fb9c8937411ca9b449b84470c6
Tobías <tobiasazcurrabi@gmail.com>
2024-08-23 13:36:14 -0300

    adfa

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from "react";
 import Datepicker, { DateValueType } from "react-tailwindcss-datepicker";
 import { RootState } from "../redux/configureStore";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,7 +8,6 @@ import {
 	setTelefonos,
 	setValueDate,
 } from "../redux/data/dataAction";
-import { useEffect } from "react";
 import { formatDate } from "../helpers/dateToday";
 import {
 	ReadDataForDateRange,
@@ -20,6 +20,9 @@ import arrow from "../assets/arrowIcon.png";
 const Calendar = () => {
 	const dispatch = useDispatch();
 	const { valueDate } = useSelector((state: RootState) => state.data);
+	const [isOpen, setIsOpen] = useState(false);
+	const calendarRef = useRef<HTMLDivElement>(null);
+
 	const handleValueDate = async (value: DateValueType) => {
 		dispatch(setValueDate(value));
 
@@ -47,6 +50,26 @@ const Calendar = () => {
 		}
 	}, [dispatch, valueDate]);
 
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				calendarRef.current &&
+				!calendarRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, []);
+
+	const handleCalendarClick = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
 		<>
 			<style>
@@ -58,12 +81,20 @@ const Calendar = () => {
 						position: absolute;
 						right: 10px;
 						top: 50%;
-						transform: translateY(-50%);
+						transform: translateY(-50%) rotate(90deg); /* Flecha apuntando hacia abajo por defecto */
 						pointer-events: none;
+						transition: transform 0.3s ease;
+					}
+					.arrow-down.open {
+						transform: translateY(-50%) rotate(-90deg); /* Flecha apuntando hacia arriba cuando está abierto */
 					}
 				`}
 			</style>
-			<div className="calendar-container">
+			<div
+				className="calendar-container"
+				ref={calendarRef}
+				onClick={handleCalendarClick}
+			>
 				<Datepicker
 					separator={"hasta"}
 					primaryColor={"red"}
@@ -79,9 +110,9 @@ const Calendar = () => {
 				/>
 				<img
 					src={arrow}
-					className="h-2 arrow-down"
+					className={`h-2 arrow-down ${isOpen ? "open" : ""}`}
 					style={{ filter: "invert(100%)" }}
-					alt="arrow down"
+					alt="arrow"
 				/>
 			</div>
 		</>
