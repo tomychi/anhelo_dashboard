8879021c20608d0f2f72e47ab4579c2ea6182d32
Tobías <tobiasazcurrabi@gmail.com>
2024-08-23 12:11:52 -0300

    dsafas

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -4,6 +4,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loginSuccess } from "../../redux/auth/authAction";
 import Sticker from "../../assets/anheloTMblack.png";
+import absoluteLogo from "../../assets/absoluteIsologo.avif";
 
 export const Login = () => {
 	const [email, setEmail] = useState("");
@@ -45,14 +46,28 @@ export const Login = () => {
 			className="font-antonio  w-full p-4 md:p-0 md:w-1/3 mx-auto text-black font-black"
 			onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleLogin(e)}
 		>
-			<img
-				src={Sticker}
-				className=" md:w-full w-full mb-4  mx-auto"
-				alt="anhelo Logo"
-			/>
+			<div className="flex flex-col ">
+				<img
+					src={Sticker}
+					className="w-3/4 mb-4 mx-auto"
+					alt="Anhelo Logo"
+					style={{ filter: "invert(100%)", WebkitFilter: "invert(100%)" }}
+				/>
+				<div className="flex flex-row justify-center mt-[-13px]">
+					<p className="text-gray-100 text-xs">powered by</p>
+					<div className="flex flex-row gap-1 items-center">
+						<img
+							src={absoluteLogo}
+							className="h-10 brightness-0 invert-[0.92] saturate-0 contrast-[0.92]"
+							alt=""
+						/>
+						<p className="text-gray-100">Absolute</p>
+					</div>
+				</div>
+			</div>
 			<div className="mb-4">
-				<label className="block mb-2 text-sm ">
-					BIENVENIDO, POR FAVOR TU CORREO
+				<label className="block mb-2 text-gray-100 text-sm ">
+					Bienvenido, por favor tu correo
 				</label>
 
 				<input
@@ -65,10 +80,12 @@ export const Login = () => {
 				/>
 			</div>
 			<div className="mb-5">
-				<label className="block mb-2 text-sm  ">TU CONTRASEÑA</label>
+				<label className="block mb-2 text-sm text-gray-100 ">
+					Y tu contraseña
+				</label>
 
 				<input
-					className="block py-2.5  w-full  texk-black  bg-transparent border-0 border-b-2 border-black appearance-none text-black focus:outline-none focus:ring-0 peer"
+					className="block py-2.5  w-full  texk-black  bg-transparent border-0 border-b-2 border-black appearance-none bg-gray-100 text-black focus:outline-none focus:ring-0 peer"
 					required
 					type="password"
 					placeholder="password"
@@ -80,7 +97,7 @@ export const Login = () => {
 
 			<button
 				type="submit"
-				className=" text-custom-red w-full p-4 bg-black font-black uppercase  outline-none"
+				className=" text-black w-full p-4 bg-gray-100 font-black uppercase  outline-none"
 			>
 				INGRESAR
 			</button>
--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -3,7 +3,7 @@ import { Login } from "../components/auth/Login";
 export const Authentication = () => {
 	return (
 		//El que lee es gay
-		<div className=" bg-custom-red w-full h-full flex items-center ">
+		<div className=" bg-black w-full h-full flex items-center ">
 			<Login />
 		</div>
 	);
