8f542b9340c6c6ef18beb92c54a596f659c9bec1
Tobías <tobiasazcurrabi@gmail.com>
2024-08-27 12:52:36 -0300

    vasd

--- src/pages/NuevaCompra.tsx
+++ src/pages/NuevaCompra.tsx
@@ -3,6 +3,9 @@ import { FormGasto } from "../components/gastos";
 export const NuevaCompra = () => {
 	return (
 		<div className="flex flex-col">
+			<div className="flex flex-row justify-between items-center mt-8 mx-4">
+				<p className="text-black font-medium text-5xl ">Nueva compra</p>
+			</div>
 			<FormGasto />
 		</div>
 	);
