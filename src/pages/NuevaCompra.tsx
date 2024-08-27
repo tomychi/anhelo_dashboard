8bc2d69f887c5e233253a79725fe651fd269343b
Tobías <tobiasazcurrabi@gmail.com>
2024-08-27 12:59:55 -0300

    fas

--- src/pages/NuevaCompra.tsx
+++ src/pages/NuevaCompra.tsx
@@ -2,11 +2,11 @@ import { FormGasto } from "../components/gastos";
 
 export const NuevaCompra = () => {
 	return (
-		<div className="flex flex-col">
+		<div className="flex flex-col bg-gray-200">
 			<div className="flex flex-row justify-between items-center mt-8 mx-4">
 				<p className="text-black font-medium text-5xl ">Nueva compra</p>
 			</div>
-			<div className="bg-gray-300 p-4 mt-4">
+			<div className="bg-gray-100 shadow-2xl p-4 mt-4 rounded-md">
 				<FormGasto />
 			</div>
 		</div>
