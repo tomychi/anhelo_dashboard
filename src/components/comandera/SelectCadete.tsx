11024767032452b583ea85dead2e9b8cfa09185d
Tobías <tobiasazcurrabi@gmail.com>
2024-09-06 15:52:22 -0300

    vds

--- src/components/comandera/SelectCadete.tsx
+++ src/components/comandera/SelectCadete.tsx
@@ -1,121 +1,121 @@
 // componente para seleccionar un cadete
 // se utiliza en el componente Comandera.tsx
-import React, { ChangeEvent, useEffect, useState } from 'react';
-import Swal from 'sweetalert2';
-import { updateCadeteForOrder } from '../../firebase/UploadOrder';
-import { readEmpleados } from '../../firebase/registroEmpleados';
-import { collection, getFirestore, onSnapshot } from 'firebase/firestore';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import { updateCadeteForOrder } from "../../firebase/UploadOrder";
+import { readEmpleados } from "../../firebase/registroEmpleados";
+import { collection, getFirestore, onSnapshot } from "firebase/firestore";
 
 interface SelectCadeteProps {
-  elaborado: boolean;
-  cadete: string;
-  fecha: string;
-  id: string;
+	elaborado: boolean;
+	cadete: string;
+	fecha: string;
+	id: string;
 }
 
 export const SelectCadete = ({
-  elaborado,
-  cadete,
-  fecha,
-  id,
+	elaborado,
+	cadete,
+	fecha,
+	id,
 }: SelectCadeteProps) => {
-  const [selectedCadete, setSelectedCadete] = useState(cadete);
-  const [cadetes, setCadetes] = useState<string[]>([]);
+	const [selectedCadete, setSelectedCadete] = useState(cadete);
+	const [cadetes, setCadetes] = useState<string[]>([]);
 
-  // Función para obtener y filtrar cadetes
-  const fetchCadetes = async () => {
-    try {
-      const empleados = await readEmpleados();
-      const cadetesDisponibles = empleados
-        .filter(
-          (empleado) => empleado.category === 'cadete' && empleado.available
-        )
-        .map((empleado) => empleado.name);
-      console.log(cadetesDisponibles);
-      setCadetes(cadetesDisponibles);
-    } catch (error) {
-      console.error('Error al obtener los empleados:', error);
-    }
-  };
+	// Función para obtener y filtrar cadetes
+	const fetchCadetes = async () => {
+		try {
+			const empleados = await readEmpleados();
+			const cadetesDisponibles = empleados
+				.filter(
+					(empleado) => empleado.category === "cadete" && empleado.available
+				)
+				.map((empleado) => empleado.name);
+			// console.log(cadetesDisponibles);
+			setCadetes(cadetesDisponibles);
+		} catch (error) {
+			console.error("Error al obtener los empleados:", error);
+		}
+	};
 
-  // Llamar a la función fetchCadetes al montar el componente y cuando se actualice
-  useEffect(() => {
-    const firestore = getFirestore();
+	// Llamar a la función fetchCadetes al montar el componente y cuando se actualice
+	useEffect(() => {
+		const firestore = getFirestore();
 
-    fetchCadetes();
+		fetchCadetes();
 
-    // Aquí podrías agregar un listener para actualizar la lista de cadetes en tiempo real
-    // Si estás utilizando Firestore, puedes agregar un listener en el useEffect
+		// Aquí podrías agregar un listener para actualizar la lista de cadetes en tiempo real
+		// Si estás utilizando Firestore, puedes agregar un listener en el useEffect
 
-    // Ejemplo:
-    const unsubscribe = onSnapshot(
-      collection(firestore, 'empleados'),
-      fetchCadetes
-    );
-    return () => unsubscribe();
-  }, []);
-  const handleCadeteChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    const nuevoCadete = event.target.value;
-    if (nuevoCadete === 'default') {
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Elegi un cadete',
-      });
-      setSelectedCadete('default');
+		// Ejemplo:
+		const unsubscribe = onSnapshot(
+			collection(firestore, "empleados"),
+			fetchCadetes
+		);
+		return () => unsubscribe();
+	}, []);
+	const handleCadeteChange = (event: ChangeEvent<HTMLSelectElement>) => {
+		const nuevoCadete = event.target.value;
+		if (nuevoCadete === "default") {
+			Swal.fire({
+				icon: "error",
+				title: "Error",
+				text: "Elegi un cadete",
+			});
+			setSelectedCadete("default");
 
-      return;
-    }
+			return;
+		}
 
-    if (nuevoCadete === 'nuevo') {
-      // El usuario eligió agregar un nuevo cadete, restablecer el estado para el nuevo cadete
-      setSelectedCadete('nuevo');
-      return;
-    }
-    setSelectedCadete(nuevoCadete);
+		if (nuevoCadete === "nuevo") {
+			// El usuario eligió agregar un nuevo cadete, restablecer el estado para el nuevo cadete
+			setSelectedCadete("nuevo");
+			return;
+		}
+		setSelectedCadete(nuevoCadete);
 
-    // Aquí debes obtener la fecha del pedido y el ID del pedido según tu implementación
+		// Aquí debes obtener la fecha del pedido y el ID del pedido según tu implementación
 
-    // Luego llama a la función para actualizar el cadete en la base de datos
-    updateCadeteForOrder(fecha, id, nuevoCadete)
-      .then(() => {
-        Swal.fire({
-          icon: 'success',
-          title: 'CADETE ASIGNADO',
-          text: `El viaje lo lleva: ${nuevoCadete} `,
-        });
-      })
-      .catch(() => {
-        console.error('Error al actualizar el cadete del pedido:');
-      });
-  };
+		// Luego llama a la función para actualizar el cadete en la base de datos
+		updateCadeteForOrder(fecha, id, nuevoCadete)
+			.then(() => {
+				Swal.fire({
+					icon: "success",
+					title: "CADETE ASIGNADO",
+					text: `El viaje lo lleva: ${nuevoCadete} `,
+				});
+			})
+			.catch(() => {
+				console.error("Error al actualizar el cadete del pedido:");
+			});
+	};
 
-  return (
-    <div className="mt-4 w-full uppercase font-black gap-2 flex flex-row justify-center">
-      <label htmlFor="cadete" className="text-white">
-        Cadete:
-      </label>
-      <select
-        id="cadete"
-        name="cadete"
-        value={selectedCadete}
-        onChange={handleCadeteChange}
-        className={` bg-white  w-full uppercase rounded-none flex flex-row gap-4 ${
-          elaborado ? 'text-green-500' : 'text-custom-red'
-        } font-black  `}
-      >
-        <option value={cadete} defaultValue={cadete}>
-          {cadete}
-        </option>
-        {cadetes.map((c, i) => {
-          if (c === cadete) return null;
-          return (
-            <option value={c} key={i}>
-              {c}
-            </option>
-          );
-        })}
-      </select>
-    </div>
-  );
+	return (
+		<div className="mt-4 w-full uppercase font-black gap-2 flex flex-row justify-center">
+			<label htmlFor="cadete" className="text-white">
+				Cadete:
+			</label>
+			<select
+				id="cadete"
+				name="cadete"
+				value={selectedCadete}
+				onChange={handleCadeteChange}
+				className={` bg-white  w-full uppercase rounded-none flex flex-row gap-4 ${
+					elaborado ? "text-green-500" : "text-custom-red"
+				} font-black  `}
+			>
+				<option value={cadete} defaultValue={cadete}>
+					{cadete}
+				</option>
+				{cadetes.map((c, i) => {
+					if (c === cadete) return null;
+					return (
+						<option value={c} key={i}>
+							{c}
+						</option>
+					);
+				})}
+			</select>
+		</div>
+	);
 };
