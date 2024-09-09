23ab237cf397ed34003a47b4d50a9dc380d9f480
Tobías <tobiasazcurrabi@gmail.com>
2024-09-09 18:31:40 -0300

    asdfas

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -379,6 +379,23 @@ export const Comandera = () => {
 								Grupo óptimo {index + 1}
 							</h3>
 						</div>
+						<div className=" w-1/3 rounded-full flex justify-center ml-4 mb-4 pt-2 pb-2 items-center bg-gray-100 gap-2 font-coolvetica ">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								className="w-6"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+
+							<p>¿Para quien?</p>
+						</div>
+
 						<div className="flex flex-col px-4  bg-gray-100  mx-4 mb-4 rounded-lg ">
 							{grupo.grupo.map((orden, ordenIndex) => (
 								<div
