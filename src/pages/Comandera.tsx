71a084708cb53859520ed1b0387eeb80e4da2f8c
Tobías <tobiasazcurrabi@gmail.com>
2024-09-09 17:50:17 -0300

    asdfas

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -409,17 +409,33 @@ export const Comandera = () => {
 							{grupo.grupo.map((orden, ordenIndex) => (
 								<div
 									key={orden.id}
-									className="pt-3 pb-4 bg-black text-gray-100  px-4"
+									className="pt-3 pb-4 flex flex-row items-center gap-2 bg-black text-gray-100  px-4"
 								>
-									<p className="font-semibold">
-										{ordenIndex + 1}. {orden.direccion.split(",")[0]}
-									</p>
-									<p className="text-xs">
-										Demora:{" "}
-										{calcularMinutosTranscurridos(orden.hora) ??
-											"Reserva futura"}{" "}
-										minutos
-									</p>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										className="h-6 w-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+										/>
+									</svg>
+									<div>
+										<p className="font-semibold">
+											{ordenIndex + 1}. {orden.direccion.split(",")[0]}
+										</p>
+										<p className="text-xs">
+											Demora:{" "}
+											{calcularMinutosTranscurridos(orden.hora) ??
+												"Reserva futura"}{" "}
+											minutos
+										</p>
+									</div>
 								</div>
 							))}
 						</div>
