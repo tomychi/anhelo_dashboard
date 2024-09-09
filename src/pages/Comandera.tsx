03c960c448907a8ffaf519ad8e4ddda507ed9461
Tobías <tobiasazcurrabi@gmail.com>
2024-09-09 18:37:52 -0300

    fasdfa

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -421,12 +421,12 @@ export const Comandera = () => {
 										viewBox="0 0 24 24"
 										stroke-width="1.5"
 										stroke="currentColor"
-										className="h-6 w-6"
+										className="h-6 "
 									>
 										<path
 											stroke-linecap="round"
 											stroke-linejoin="round"
-											d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+											d="M3.75 9h16.5m-16.5 6.75h16.5"
 										/>
 									</svg>
 								</div>
