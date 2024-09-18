a5910766a0bc28818655dc1965b0b12937a05930
Tobías <tobiasazcurrabi@gmail.com>
2024-09-18 18:46:56 -0300

    fda

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -75,7 +75,7 @@ export const Sidebar = () => {
 					<button className="bg-gray-100 rounded-full flex items-center pt-3 pb-4 pl-3 pr-4 h-9 focus:outline-none transition duration-300 ease-in-out hover:bg-gray-300">
 						<p className=" font-medium font-coolvetica ">Accion rapida</p>
 					</button>
-					<div className="bg-gray-100 h-9 w-9 ml-2 rounded-full justify-center items-center flex font-coolvetica font-bold">
+					<div className="bg-gray-100 h-9 w-9 ml-4 rounded-full justify-center items-center flex font-coolvetica font-bold">
 						TA
 					</div>
 				</div>
