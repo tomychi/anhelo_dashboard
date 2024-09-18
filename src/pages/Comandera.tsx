73f83d462eb46c8895152e6579d25e6d3d0033c8
Tobías <tobiasazcurrabi@gmail.com>
2024-09-18 17:30:12 -0300

    fdaf

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -1043,14 +1043,14 @@ export const Comandera: React.FC = () => {
 									xmlns="http://www.w3.org/2000/svg"
 									viewBox="0 0 24 24"
 									fill="currentColor"
-									className="w-6 absolute left-3 top-1/2 -translate-y-1/2"
+									className="h-6 absolute left-3 top-1/2  -translate-y-1/2"
 									style={{
 										filter: "invert(100%)",
 									}}
 								>
 									<path
 										fill-rule="evenodd"
-										d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+										d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
 										clip-rule="evenodd"
 									/>
 								</svg>
