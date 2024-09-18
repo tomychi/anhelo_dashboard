94026e7762f06a176c69170a7361a950d97a3e67
Tobías <tobiasazcurrabi@gmail.com>
2024-09-18 17:51:13 -0300

    fdasfa

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -1796,9 +1796,9 @@ export const Comandera: React.FC = () => {
 								);
 							})
 						) : (
-							<p className="mt-[-8px] text-left w-full">
-								Si hay pedidos para agrupar los veras aca.
-							</p>
+							<div className="bg-gray-300 h-min pt-2 pb-2 items-center  shadow-black shadow-md font-coolvetica  flex justify-center rounded-lg opacity-50 text-center  ">
+								Si hay pedidos para agrupar los veras aca...
+							</div>
 						)}
 					</div>
 				</div>
