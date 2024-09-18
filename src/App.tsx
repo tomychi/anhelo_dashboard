765b5c2e4c4e67bd48845c5e57d77ffd20d4ebcb
Tobías <tobiasazcurrabi@gmail.com>
2024-09-18 18:33:51 -0300

    fadfa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { Navigation } from './routes/Navigation';
+import React, { useState, useEffect } from "react";
+import { Navigation } from "./routes/Navigation";
+import SplashScreen from "./components/SplashScreen"; // Asegúrate de que la ruta sea correcta
 
 function App() {
-  return (
-    <>
-      <Navigation />
-    </>
-  );
+	const [showSplash, setShowSplash] = useState(true);
+
+	useEffect(() => {
+		// Simulamos una carga de datos o inicialización
+		const timer = setTimeout(() => {
+			setShowSplash(false);
+		}, 3000); // 3 segundos de duración del splash screen
+
+		return () => clearTimeout(timer);
+	}, []);
+
+	if (showSplash) {
+		return <SplashScreen onFinish={() => setShowSplash(false)} />;
+	}
+
+	return (
+		<>
+			<Navigation />
+		</>
+	);
 }
 
 export default App;
