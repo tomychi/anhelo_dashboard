80882dceed0b6b467a6ccd755a30461a5f835584
Tobías <tobiasazcurrabi@gmail.com>
2024-09-19 16:09:11 -0300

    fdafa

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -1556,7 +1556,20 @@ export const Comandera: React.FC = () => {
 													"Deshacer"
 												)}
 											</button>
-											<div className="relative mt-8 mb-2">
+											<div className="relative mt-8 mb-2 flex items-center">
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 24 24"
+													fill="currentColor"
+													className="h-6 absolute left-24 "
+												>
+													<path
+														fill-rule="evenodd"
+														d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+														clip-rule="evenodd"
+													/>
+												</svg>
+
 												<select
 													className="bg-gray-100 appearance-none w-full text-center py-2 rounded-full"
 													style={{
