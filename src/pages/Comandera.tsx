aab0a2503436963c7a30dc318fbff9f8f79d3ff2
Tobías <tobiasazcurrabi@gmail.com>
2024-09-18 23:17:21 -0300

    gdsgds

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -1876,15 +1876,19 @@ export const Comandera: React.FC = () => {
 						)}
 					</div>
 					{modalIsOpen && selectedPedido && (
-						<div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
-							<div className="bg-white p-6 rounded-lg max-w-lg  max-h-[90vh] overflow-y-auto">
-								<CardComanda {...selectedPedido} cadetes={cadetes} />
-								<button
-									onClick={() => setModalIsOpen(false)}
-									className="mt-4 bg-black text-white px-4 py-2 rounded"
-								>
-									Cerrar
-								</button>
+						<div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center overflow-y-auto">
+							<div className="relative bg-white rounded-lg w-full max-w-lg m-4 max-h-[90vh] flex flex-col">
+								<div className="overflow-y-auto p-6 flex-grow">
+									<CardComanda {...selectedPedido} cadetes={cadetes} />
+								</div>
+								<div className="sticky bottom-0 bg-white p-4 border-t">
+									<button
+										onClick={() => setModalIsOpen(false)}
+										className="w-full bg-black text-white px-4 py-2 rounded"
+									>
+										Cerrar
+									</button>
+								</div>
 							</div>
 						</div>
 					)}
