a5ae8468ac794a38fcfb274bd79526eb6a19432f
Tobías <tobiasazcurrabi@gmail.com>
2024-09-19 14:40:10 -0300

    dasca

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -920,6 +920,10 @@ export const Comandera: React.FC = () => {
 		setModalIsOpen(true);
 	};
 
+	const [starTooltipVisibility, setStarTooltipVisibility] = useState<
+		Record<string, boolean>
+	>({});
+
 	return (
 		<>
 			<style>
@@ -1698,7 +1702,19 @@ export const Comandera: React.FC = () => {
 																		onClick={() =>
 																			togglePedidoPrioritario(pedido)
 																		}
-																		className="ml-2 p-1 rounded-full"
+																		className="ml-2 p-1 rounded-full relative"
+																		onMouseEnter={() =>
+																			setStarTooltipVisibility((prev) => ({
+																				...prev,
+																				[pedido.id]: true,
+																			}))
+																		}
+																		onMouseLeave={() =>
+																			setStarTooltipVisibility((prev) => ({
+																				...prev,
+																				[pedido.id]: false,
+																			}))
+																		}
 																	>
 																		{pedidosPrioritarios.some(
 																			(p) => p.id === pedido.id
@@ -1730,6 +1746,16 @@ export const Comandera: React.FC = () => {
 																					d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
 																				/>
 																			</svg>
+																		)}
+																		{starTooltipVisibility[pedido.id] && (
+																			<div className="absolute z-50 px-2 py-2 font-light text-white bg-black rounded-lg shadow-sm tooltip text-xs bottom-full left-1/2 transform -translate-x-1/2 mb-2 whitespace-nowrap flex flex-row items-center gap-2 h-[30px]">
+																				<p className="mb-[1.5px] text-xs font-black">
+																					1ro
+																				</p>
+																				<p className="mb-[1.5px] text-xs">
+																					Presiona para priorizar este pedido
+																				</p>
+																			</div>
 																		)}
 																	</button>
 																	<svg
@@ -1938,7 +1964,19 @@ export const Comandera: React.FC = () => {
 														<div className="flex items-center relative">
 															<button
 																onClick={() => togglePedidoPrioritario(pedido)}
-																className="ml-2 p-1 rounded-full"
+																className="ml-2 p-1 rounded-full relative"
+																onMouseEnter={() =>
+																	setStarTooltipVisibility((prev) => ({
+																		...prev,
+																		[`process-${pedido.id}`]: true,
+																	}))
+																}
+																onMouseLeave={() =>
+																	setStarTooltipVisibility((prev) => ({
+																		...prev,
+																		[`process-${pedido.id}`]: false,
+																	}))
+																}
 															>
 																{pedidosPrioritarios.some(
 																	(p) => p.id === pedido.id
@@ -1947,7 +1985,7 @@ export const Comandera: React.FC = () => {
 																		xmlns="http://www.w3.org/2000/svg"
 																		viewBox="0 0 24 24"
 																		fill="currentColor"
-																		className=" w-4   text-red-main"
+																		className="w-4 text-red-main"
 																	>
 																		<path
 																			fillRule="evenodd"
@@ -1970,6 +2008,18 @@ export const Comandera: React.FC = () => {
 																			d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
 																		/>
 																	</svg>
+																)}
+																{starTooltipVisibility[
+																	`process-${pedido.id}`
+																] && (
+																	<div className="absolute z-50 px-2 py-2 font-light text-white bg-black rounded-lg shadow-sm tooltip text-xs bottom-full left-1/2 transform -translate-x-1/2 mb-2 whitespace-nowrap flex flex-row items-center gap-2 h-[30px]">
+																		<p className="mb-[1.5px] text-xs font-black">
+																			1ro
+																		</p>
+																		<p className="mb-[1.5px] text-xs">
+																			Presiona para priorizar este pedido
+																		</p>
+																	</div>
 																)}
 															</button>
 															<svg
