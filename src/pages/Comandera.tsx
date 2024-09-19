86d8f811cb06b1d4e517da8a6cbedb6f5b5b1fb0
Tobías <tobiasazcurrabi@gmail.com>
2024-09-19 16:41:15 -0300

    fdafa

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -1823,8 +1823,23 @@ export const Comandera: React.FC = () => {
 																		)}
 																		{starTooltipVisibility[pedido.id] && (
 																			<div className="absolute z-50 px-2 py-2 font-light text-white bg-black rounded-lg shadow-sm tooltip text-xs bottom-full left-1/2 transform -translate-x-1/2 mb-2 whitespace-nowrap flex flex-row items-center gap-2 h-[30px]">
+																				<svg
+																					xmlns="http://www.w3.org/2000/svg"
+																					fill="none"
+																					viewBox="0 0 24 24"
+																					strokeWidth="1.5"
+																					stroke="currentColor"
+																					className="w-3 h-3"
+																				>
+																					<path
+																						strokeLinecap="round"
+																						strokeLinejoin="round"
+																						d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"
+																					/>
+																				</svg>
 																				<p className="mb-[1.5px] text-xs">
-																					Presiona para priorizar este pedido
+																					Para priorizar un pedido debes
+																					deshacer el grupo
 																				</p>
 																			</div>
 																		)}
