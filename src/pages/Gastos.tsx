046d226b68c96e0621e8411e72fffad4636afce7
Tobías <tobiasazcurrabi@gmail.com>
2024-09-24 20:32:24 -0300

    fdafa

--- src/pages/Gastos.tsx
+++ src/pages/Gastos.tsx
@@ -149,6 +149,7 @@ export const Gastos: React.FC = () => {
 					<p className="font-bold">Nueva compra</p>
 				</NavLink>
 			</div>
+
 			<div className="p-4">
 				<Calendar />
 				<div className="flex flex-row gap-2 mt-2">
@@ -174,7 +175,15 @@ export const Gastos: React.FC = () => {
 									d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
 								/>
 							</svg>
-							<p>{selectedCategory}</p>
+							<p
+								className={
+									selectedCategory === "Todos"
+										? " text-black text-opacity-40"
+										: ""
+								}
+							>
+								{selectedCategory}
+							</p>
 						</div>
 						<img
 							src={arrow}
@@ -183,7 +192,7 @@ export const Gastos: React.FC = () => {
 							onClick={() => setShowCategoryDropdown(!showCategoryDropdown)}
 						/>
 						{showCategoryDropdown && (
-							<div className="absolute top-full left-0 w-full bg-white border border-gray-300 rounded-md shadow-lg z-10">
+							<div className="absolute top-full left-0 w-full bg-gray-100 border border-gray-300 rounded-md shadow-lg z-10">
 								{uniqueCategories.map((category) => (
 									<div
 										key={category}
@@ -200,6 +209,7 @@ export const Gastos: React.FC = () => {
 							</div>
 						)}
 					</div>
+
 					<div className="flex items-center w-2/3 h-10 gap-1 rounded-lg border-4 border-black focus:ring-0 font-coolvetica text-black text-xs font-light">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
