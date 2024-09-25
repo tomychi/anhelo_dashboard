5f62f6462e3723effe12a4bb478cf17e004cc4f9
Tobías <tobiasazcurrabi@gmail.com>
2024-09-24 22:55:53 -0300

    fdafa

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -66,7 +66,7 @@ export const Sidebar = () => {
 							</div>
 						)}
 					</button>
-					<button className="bg-gray-100 rounded-full flex items-center pt-3 pb-4 pl-3 pr-4 h-9 focus:outline-none transition duration-300 ease-in-out hover:bg-gray-300">
+					{/* <button className="bg-gray-100 rounded-full flex items-center pt-3 pb-4 pl-3 pr-4 h-9 focus:outline-none transition duration-300 ease-in-out hover:bg-gray-300">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							viewBox="0 0 24 24"
@@ -77,7 +77,7 @@ export const Sidebar = () => {
 						</svg>
 
 						<p className=" font-bold font-coolvetica ">Accion rapida</p>
-					</button>
+					</button> */}
 					<div className="relative bg-gray-100 h-9 w-9 ml-2 rounded-full justify-center items-center flex font-coolvetica font-bold focus:outline-none transition duration-300 ease-in-out hover:bg-gray-300 cursor-pointer">
 						TA
 						<div className="absolute top-0 right-0 h-2 w-2 bg-red-500 rounded-full animate-pulse"></div>
