1a5e8ff33b360df7ced6d89ce3ec9ae489580eec
Tobías <tobiasazcurrabi@gmail.com>
2024-11-21 21:45:52 -0300

    gsdfgs

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -1070,7 +1070,38 @@ export const Comandera: React.FC = () => {
 		}
 	};
 
-	console.log(gruposOptimos);
+	console.log(pedidosPorHacer);
+
+	useEffect(() => {
+		if (orders.length > 0 && gruposListos.length > 0) {
+			const gruposActualizados = gruposListos.map((grupo) => ({
+				...grupo,
+				pedidos: grupo.pedidos.map((pedido) => {
+					// Buscar la orden actualizada correspondiente
+					const ordenActualizada = orders.find((o) => o.id === pedido.id);
+					if (ordenActualizada) {
+						return {
+							...pedido,
+							elaborado: ordenActualizada.elaborado,
+						};
+					}
+					return pedido;
+				}),
+			}));
+
+			// Solo actualizar si hay cambios en el estado de elaboración
+			const hayCambios = gruposActualizados.some((grupo, i) =>
+				grupo.pedidos.some(
+					(pedido, j) =>
+						pedido.elaborado !== gruposListos[i].pedidos[j].elaborado
+				)
+			);
+
+			if (hayCambios) {
+				setGruposListos(gruposActualizados);
+			}
+		}
+	}, [orders]);
 
 	return (
 		<>
@@ -1844,6 +1875,7 @@ export const Comandera: React.FC = () => {
 													"Deshacer"
 												)}
 											</button>
+
 											<div className="relative mt-8 mb-2 flex items-center">
 												<svg
 													xmlns="http://www.w3.org/2000/svg"
@@ -1895,6 +1927,28 @@ export const Comandera: React.FC = () => {
 													</div>
 												)}
 											</div>
+											{grupo.pedidos.some(
+												(pedido) =>
+													pedido.hasOwnProperty("elaborado") &&
+													!pedido.elaborado
+											) && (
+												<div className="bg-gray-400 bg-opacity-50 w-full h-10 mb-2 text-red-main gap-2 rounded-full flex justify-center items-center font-coolvetica">
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 24 24"
+														fill="currentColor"
+														className="h-6"
+													>
+														<path d="M5.85 3.5a.75.75 0 0 0-1.117-1 9.719 9.719 0 0 0-2.348 4.876.75.75 0 0 0 1.479.248A8.219 8.219 0 0 1 5.85 3.5ZM19.267 2.5a.75.75 0 1 0-1.118 1 8.22 8.22 0 0 1 1.987 4.124.75.75 0 0 0 1.48-.248A9.72 9.72 0 0 0 19.266 2.5Z" />
+														<path
+															fillRule="evenodd"
+															d="M12 2.25A6.75 6.75 0 0 0 5.25 9v.75a8.217 8.217 0 0 1-2.119 5.52.75.75 0 0 0 .298 1.206c1.544.57 3.16.99 4.831 1.243a3.75 3.75 0 1 0 7.48 0 24.583 24.583 0 0 0 4.83-1.244.75.75 0 0 0 .298-1.205 8.217 8.217 0 0 1-2.118-5.52V9A6.75 6.75 0 0 0 12 2.25ZM9.75 18c0-.034 0-.067.002-.1a25.05 25.05 0 0 0 4.496 0l.002.1a2.25 2.25 0 1 1-4.5 0Z"
+															clipRule="evenodd"
+														/>
+													</svg>
+													<p>Enviar a cocinar YA</p>
+												</div>
+											)}
 											{grupo.pedidos.map((pedido, pedidoIndex) => (
 												<Draggable
 													key={`manual-${pedido.id}-xd`}
@@ -2521,19 +2575,6 @@ export const Comandera: React.FC = () => {
 							}
 							cadetes={cadetes}
 						/>
-						<div className="mt-2">
-							{seccionActiva === "mapa" &&
-								(location.pathname === "/comandas" ? (
-									<DeliveryMap
-										orders={[...pedidosHechos, ...pedidosPorHacer]}
-									/>
-								) : (
-									<DeliveryMap orders={orders} />
-								))}
-						</div>
-						<div className="mt-2">
-							{seccionActiva === "registro" && <RegistroEmpleado />}
-						</div>
 					</>
 				)}
 			</div>
