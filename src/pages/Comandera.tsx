bc61a029b14fe9b51bfae102f1585d03eb8d17b9
Tobías <tobiasazcurrabi@gmail.com>
2024-11-24 21:36:25 -0300

    dasfas

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -1395,7 +1395,7 @@ export const Comandera: React.FC = () => {
 											: "bg-black text-white hover:bg-gray-800"
 									}`}
 									style={{
-										width: "105px",
+										width: "110px",
 									}}
 								>
 									<svg
@@ -1406,7 +1406,7 @@ export const Comandera: React.FC = () => {
 									>
 										<path d="M15 6.75a.75.75 0 0 0-.75.75V18a.75.75 0 0 0 .75.75h.75a.75.75 0 0 0 .75-.75V7.5a.75.75 0 0 0-.75-.75H15ZM20.25 6.75a.75.75 0 0 0-.75.75V18c0 .414.336.75.75.75H21a.75.75 0 0 0 .75-.75V7.5a.75.75 0 0 0-.75-.75h-.75ZM5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256L5.055 7.061Z" />
 									</svg>
-									<p className="font-bold">Pausa</p>
+									<p className="font-bold">Pausar</p>
 								</button>
 							)}
 
