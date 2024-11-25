70254eec2e5148b064f0c7a82c4da8b09925afcb
Tobías <tobiasazcurrabi@gmail.com>
2024-11-24 22:59:01 -0300

    dasf

--- src/pages/Comandera.tsx
+++ src/pages/Comandera.tsx
@@ -2042,14 +2042,16 @@ export const Comandera: React.FC = () => {
 													<div className="relative  flex  items-center">
 														<svg
 															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
 															viewBox="0 0 24 24"
-															fill="currentColor"
-															className="h-6 absolute left-[10px] text-red-main"
+															stroke-width="1.5"
+															stroke="currentColor"
+															className="h-6 absolute left-4 text-red-main"
 														>
 															<path
-																fillRule="evenodd"
-																clipRule="evenodd"
-																d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+																stroke-linecap="round"
+																stroke-linejoin="round"
+																d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
 															/>
 														</svg>
 
@@ -2058,7 +2060,7 @@ export const Comandera: React.FC = () => {
 															style={{
 																WebkitAppearance: "none",
 																MozAppearance: "none",
-																width: "170px",
+																width: "190px",
 															}}
 															onChange={(e) =>
 																handleAsignarCadete(index, e.target.value, true)
