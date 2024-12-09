caa4d4b404e50ab3b611ad48987bcb5acccb5852
Tobías <tobiasazcurrabi@gmail.com>
2024-12-08 23:37:45 -0300

    fdas

--- src/components/vouchers/VoucherList.tsx
+++ src/components/vouchers/VoucherList.tsx
@@ -430,7 +430,6 @@ export const VoucherList: React.FC = () => {
 		if (total === 0) return "bg-red-main";
 
 		const percentage = (usados / total) * 100;
-		console.log(`Calculando porcentaje para el color: ${percentage}%`);
 
 		if (percentage < 5) return "bg-red-main";
 		if (percentage < 10) return "bg-yellow-500";
@@ -538,9 +537,20 @@ export const VoucherList: React.FC = () => {
 									<td className="w-1/12 font-bold pl-4 pr-4">
 										<button
 											onClick={() => handleVoucherSelect(t.titulo)}
-											className="px-2 py-1 rounded-full text-center text-gray-100 bg-black w-full"
+											className="px-2 py-1 rounded-full text-center text-gray-100 bg-gray-300 w-full"
 										>
-											PDF
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 24 24"
+												fill="currentColor"
+												className="h-4 text-black"
+											>
+												<path
+													fill-rule="evenodd"
+													d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 0 0 3 3h.27l-.155 1.705A1.875 1.875 0 0 0 7.232 22.5h9.536a1.875 1.875 0 0 0 1.867-2.045l-.155-1.705h.27a3 3 0 0 0 3-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0 0 18 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM16.5 6.205v-2.83A.375.375 0 0 0 16.125 3h-8.25a.375.375 0 0 0-.375.375v2.83a49.353 49.353 0 0 1 9 0Zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 0 1-.374.409H7.232a.375.375 0 0 1-.374-.409l.526-5.784a.373.373 0 0 1 .333-.337 41.741 41.741 0 0 1 8.566 0Zm.967-3.97a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H18a.75.75 0 0 1-.75-.75V10.5ZM15 9.75a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V10.5a.75.75 0 0 0-.75-.75H15Z"
+													clip-rule="evenodd"
+												/>
+											</svg>
 										</button>
 									</td>
 								</tr>
