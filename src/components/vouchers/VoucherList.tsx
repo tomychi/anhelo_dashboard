cf5c3abc09f0cdb135cab7e141bc464a0e80b622
Tobías <tobiasazcurrabi@gmail.com>
2024-12-09 19:20:26 -0300

    fdafad

--- src/components/vouchers/VoucherList.tsx
+++ src/components/vouchers/VoucherList.tsx
@@ -6,8 +6,11 @@ import {
 	VoucherTituloConFecha,
 } from "../../firebase/voucher";
 import { jsPDF } from "jspdf";
+import { projectAuth } from "../../firebase/config";
+
 import voucherImg from "../../assets/Voucher.jpg";
 import arrow from "../../assets/arrowIcon.png";
+import { NavLink } from "react-router-dom";
 
 const TableLoadingRow = () => {
 	return (
@@ -467,6 +470,10 @@ export const VoucherList: React.FC = () => {
 		}
 	};
 
+	const currentUserEmail = projectAuth.currentUser?.email;
+	const isMarketingUser = currentUserEmail === "marketing@anhelo.com";
+	console.log("acaa", isMarketingUser);
+
 	return (
 		<div className="font-coolvetica">
 			<table className="w-full text-xs text-left text-black">
@@ -537,25 +544,34 @@ export const VoucherList: React.FC = () => {
 									>
 										{t.usados} / {t.creados}
 									</td>
-									<td className="w-1/12 pl-4 pr-4">
-										<button
-											onClick={() => handleVoucherSelect(t.titulo)}
-											className="flex justify-end w-full"
-										>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 24 24"
-												fill="currentColor"
-												className="h-6 text-black"
+									{isMarketingUser ? (
+										<td className="w-1/12 pl-4 pr-4">
+											<button
+												onClick={() => handleVoucherSelect(t.titulo)}
+												className="flex justify-end w-full"
 											>
-												<path
-													fill-rule="evenodd"
-													d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 0 0 3 3h.27l-.155 1.705A1.875 1.875 0 0 0 7.232 22.5h9.536a1.875 1.875 0 0 0 1.867-2.045l-.155-1.705h.27a3 3 0 0 0 3-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0 0 18 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM16.5 6.205v-2.83A.375.375 0 0 0 16.125 3h-8.25a.375.375 0 0 0-.375.375v2.83a49.353 49.353 0 0 1 9 0Zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 0 1-.374.409H7.232a.375.375 0 0 1-.374-.409l.526-5.784a.373.373 0 0 1 .333-.337 41.741 41.741 0 0 1 8.566 0Zm.967-3.97a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H18a.75.75 0 0 1-.75-.75V10.5ZM15 9.75a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V10.5a.75.75 0 0 0-.75-.75H15Z"
-													clip-rule="evenodd"
-												/>
-											</svg>
-										</button>
-									</td>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													viewBox="0 0 24 24"
+													fill="currentColor"
+													className="h-6 text-black"
+												>
+													<path
+														fill-rule="evenodd"
+														d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 0 0 3 3h.27l-.155 1.705A1.875 1.875 0 0 0 7.232 22.5h9.536a1.875 1.875 0 0 0 1.867-2.045l-.155-1.705h.27a3 3 0 0 0 3-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0 0 18 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM16.5 6.205v-2.83A.375.375 0 0 0 16.125 3h-8.25a.375.375 0 0 0-.375.375v2.83a49.353 49.353 0 0 1 9 0Zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 0 1-.374.409H7.232a.375.375 0 0 1-.374-.409l.526-5.784a.373.373 0 0 1 .333-.337 41.741 41.741 0 0 1 8.566 0Zm.967-3.97a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H18a.75.75 0 0 1-.75-.75V10.5ZM15 9.75a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V10.5a.75.75 0 0 0-.75-.75H15Z"
+														clip-rule="evenodd"
+													/>
+												</svg>
+											</button>
+										</td>
+									) : (
+										<NavLink
+											to="/ayuda"
+											className="w-1/12 pl-4 pr-4 h-6 flex items-center"
+										>
+											<p className="text-5xl h-6 mb-10 ">...</p>
+										</NavLink>
+									)}
 								</tr>
 							);
 						})
