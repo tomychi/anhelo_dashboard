605bcf8ccefbf81d04e1b8acd73362e1c3af717a
Tobías <tobiasazcurrabi@gmail.com>
2024-12-17 06:48:44 -0300

    sdafasdf

--- src/components/deuda/PaymentTimeline.tsx
+++ src/components/deuda/PaymentTimeline.tsx
@@ -34,29 +34,31 @@ const TimelineRange = ({
 			}}
 		>
 			<div className="w-full flex  flex-col justify-start">
-				<button
-					onClick={(e) => {
-						e.stopPropagation();
-						onDelete();
-					}}
-					className="text-gray-100  "
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						className="h-4 mb-0.5"
+				<div className="flex flex-row gap-1 mb-1">
+					<button
+						onClick={(e) => {
+							e.stopPropagation();
+							onDelete();
+						}}
+						className="text-gray-100  "
 					>
-						<path
-							fill-rule="evenodd"
-							d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</button>
-				<p className="text-white font-bold text-xs truncate">
-					{formatInvestorName(investment.investorId)}
-				</p>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							className="h-4 "
+						>
+							<path
+								fill-rule="evenodd"
+								d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
+					<p className="text-white font-bold text-xs truncate">
+						{formatInvestorName(investment.investorId)}
+					</p>
+				</div>
 				<p className="text-white text-xs opacity-75">
 					${investment.monto} {investment.moneda}
 				</p>
