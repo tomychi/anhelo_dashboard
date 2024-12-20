5fe3a5b8893fe7cbd208438d011db49dc781013f
Tobías <tobiasazcurrabi@gmail.com>
2024-12-20 03:02:41 -0300

    fas

--- src/pages/DeudaManager.tsx
+++ src/pages/DeudaManager.tsx
@@ -528,7 +528,7 @@ export const DeudaManager: React.FC = () => {
 					<p className="font-bold ">Nueva inversión</p>
 				</button>
 			</div>
-			<div className=" mb-8 md:flex md:justify-center">
+			<div className=" mb-5 md:flex md:justify-center">
 				<PaymentTimeline investors={inversores} />
 			</div>
 
