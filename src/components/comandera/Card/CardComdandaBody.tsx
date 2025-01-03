18ac2bd7d11e76637b48c8d3cd0478965edd4a3c
Tobías <tobiasazcurrabi@gmail.com>
2025-01-02 23:04:54 -0300

    Fondo violeta para los agergados

--- src/components/comandera/Card/CardComdandaBody.tsx
+++ src/components/comandera/Card/CardComdandaBody.tsx
@@ -1,54 +1,68 @@
-import { DetallePedidoItem } from '../../../types/types';
+import { DetallePedidoItem } from "../../../types/types";
 
 interface CardComdandaBodyProps {
-  aclaraciones: string;
-  detallePedido: DetallePedidoItem[];
+	aclaraciones: string;
+	detallePedido: DetallePedidoItem[];
 }
 
 export const CardComdandaBody = ({
-  aclaraciones,
-  detallePedido,
+	aclaraciones,
+	detallePedido,
 }: CardComdandaBodyProps) => {
-  return (
-    <div className=" mt-8">
-      {aclaraciones && (
-        <p className="w-full mt-8 bg-black  pr-1 pl-1 pb-1 text-4xl text-center text-green-500 font-black">
-          {aclaraciones}
-        </p>
-      )}
-      {detallePedido.map(
-        (
-          {
-            burger,
-            toppings,
-            quantity,
-          }: { burger: string; toppings: string[]; quantity: number },
-          i: number
-        ) => (
-          <div key={i} className="flex mt-4 items-center flex-col">
-            <p className="text-black text-4xl font-black border-4 w-full text-center border-black pr-1 pl-1 pb-1">
-              {quantity}X {burger}
-            </p>
-            <div>
-              <div className="flex flex-col items-center ">
-                {toppings.map((topping: string, toppingIndex: number) => (
-                  <span
-                    key={`${topping}-${toppingIndex}`}
-                    className={`text-2xl flex  text-black font-black ${
-                      topping.toLowerCase() === 'huevo' ||
-                      topping.toLowerCase() === 'carne'
-                        ? 'bg-black mt-4  text-2xl text-center text-green-500'
-                        : ''
-                    }`}
-                  >
-                    {topping}
-                  </span>
-                ))}
-              </div>
-            </div>
-          </div>
-        )
-      )}
-    </div>
-  );
+	// Filter the products to show only non-extra items or confirmed extra items
+	const filteredPedido = detallePedido.filter(
+		(item) => !item.extra || (item.extra && item.isConfirmed)
+	);
+
+	return (
+		<div className="mt-8">
+			{aclaraciones && (
+				<p className="w-full mt-8 bg-black pr-1 pl-1 pb-1 text-4xl text-center text-green-500 font-black">
+					{aclaraciones}
+				</p>
+			)}
+			{filteredPedido.map(
+				(
+					{
+						burger,
+						toppings,
+						quantity,
+						extra,
+					}: {
+						burger: string;
+						toppings: string[];
+						quantity: number;
+						extra?: boolean;
+					},
+					i: number
+				) => (
+					<div key={i} className="flex mt-4 items-center flex-col">
+						<p
+							className={`text-black text-4xl font-black border-4 w-full text-center border-black pr-1 pl-1 pb-1 
+              ${extra ? "bg-purple-300" : ""}`}
+						>
+							{quantity}X {burger}
+						</p>
+						<div>
+							<div className="flex flex-col items-center">
+								{toppings.map((topping: string, toppingIndex: number) => (
+									<span
+										key={`${topping}-${toppingIndex}`}
+										className={`text-2xl flex text-black font-black ${
+											topping.toLowerCase() === "huevo" ||
+											topping.toLowerCase() === "carne"
+												? "bg-black mt-4 text-2xl text-center text-green-500"
+												: ""
+										}`}
+									>
+										{topping}
+									</span>
+								))}
+							</div>
+						</div>
+					</div>
+				)
+			)}
+		</div>
+	);
 };
--- src/components/comandera/OrderSection.tsx
+++ src/components/comandera/OrderSection.tsx
@@ -1,79 +1,91 @@
+import React, { useEffect, useState } from "react";
 import { PedidoProps } from "../../types/types";
 import { CardComanda } from "./Card/CardComanda";
+import { ReadOrdersForToday } from "../../firebase/ReadData";
 
 interface OrderSectionProps {
-  orders: PedidoProps[];
-  cadetes: string[];
+	cadetes: string[];
 }
 
-export const OrderSection: React.FC<OrderSectionProps> = ({
-  orders,
-  cadetes,
-}) => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {orders.map(
-        ({
-          aclaraciones,
-          detallePedido,
-          direccion,
-          elaborado,
-          envio,
-          fecha,
-          hora,
-          metodoPago,
-          subTotal,
-          telefono,
-          total,
-          referencias,
-          id,
-          ubicacion,
-          paid,
-          cadete,
-          dislike,
-          delay,
-          tiempoElaborado,
-          tiempoEntregado,
-          entregado,
-          map,
-          kms,
-          minutosDistancia,
-          efectivoCantidad,
-          mercadopagoCantidad,
-        }) => (
-          <div key={`comanda-${hora}-${id}`}>
-            <CardComanda
-              aclaraciones={aclaraciones}
-              direccion={direccion}
-              hora={hora}
-              metodoPago={metodoPago}
-              total={total}
-              paid={paid}
-              telefono={telefono}
-              detallePedido={detallePedido}
-              elaborado={elaborado}
-              referencias={referencias}
-              ubicacion={ubicacion}
-              fecha={fecha}
-              cadete={cadete}
-              efectivoCantidad={efectivoCantidad}
-              mercadopagoCantidad={mercadopagoCantidad}
-              tiempoElaborado={tiempoElaborado}
-              tiempoEntregado={tiempoEntregado}
-              entregado={entregado}
-              id={id}
-              subTotal={subTotal}
-              dislike={dislike}
-              delay={delay}
-              map={map}
-              kms={kms}
-              minutosDistancia={minutosDistancia}
-              envio={envio}
-              cadetes={cadetes}
-            />
-          </div>
-        ),
-      )}
-    </div>
-  );
+export const OrderSection: React.FC<OrderSectionProps> = ({ cadetes }) => {
+	const [orders, setOrders] = useState<PedidoProps[]>([]);
+
+	useEffect(() => {
+		// Suscribirse a los cambios en tiempo real
+		const unsubscribe = ReadOrdersForToday((updatedOrders) => {
+			setOrders(updatedOrders);
+		});
+
+		// Limpiar la suscripción cuando el componente se desmonte
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+			{orders.map(
+				({
+					aclaraciones,
+					detallePedido,
+					direccion,
+					elaborado,
+					envio,
+					fecha,
+					hora,
+					metodoPago,
+					subTotal,
+					telefono,
+					total,
+					referencias,
+					id,
+					ubicacion,
+					paid,
+					cadete,
+					dislike,
+					delay,
+					tiempoElaborado,
+					tiempoEntregado,
+					entregado,
+					map,
+					kms,
+					minutosDistancia,
+					efectivoCantidad,
+					mercadopagoCantidad,
+				}) => (
+					<div key={`comanda-${hora}-${id}`}>
+						<CardComanda
+							aclaraciones={aclaraciones}
+							direccion={direccion}
+							hora={hora}
+							metodoPago={metodoPago}
+							total={total}
+							paid={paid}
+							telefono={telefono}
+							detallePedido={detallePedido}
+							elaborado={elaborado}
+							referencias={referencias}
+							ubicacion={ubicacion}
+							fecha={fecha}
+							cadete={cadete}
+							efectivoCantidad={efectivoCantidad}
+							mercadopagoCantidad={mercadopagoCantidad}
+							tiempoElaborado={tiempoElaborado}
+							tiempoEntregado={tiempoEntregado}
+							entregado={entregado}
+							id={id}
+							subTotal={subTotal}
+							dislike={dislike}
+							delay={delay}
+							map={map}
+							kms={kms}
+							minutosDistancia={minutosDistancia}
+							envio={envio}
+							cadetes={cadetes}
+						/>
+					</div>
+				)
+			)}
+		</div>
+	);
 };
