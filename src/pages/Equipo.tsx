1a8b57f463de7f1cfb7a027d3fb1028e2ef1c93f
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-01-23 01:34:53 -0300

    hk

--- src/pages/Equipo.tsx
+++ src/pages/Equipo.tsx
@@ -29,7 +29,7 @@ const QRComponent: React.FC<{ employeeId: string, name: string }> = ({ employeeI
   useEffect(() => {
     if (qrRef.current) {
       const qr = qrcode(0, 'L');
-      const data = `http://192.168.100.67:5173/scan?data=${encodeURIComponent(
+      const data = `http://192.168.0.6:5173/scan?data=${encodeURIComponent(
         JSON.stringify({
           type: 'employee_scan',
           id: employeeId,
