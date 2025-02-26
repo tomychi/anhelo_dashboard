fe21a2891939bd81f3d695ee0b22eb80f612a453
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-25 23:31:50 -0300

    njk

--- src/components/facturacion/FacturaForm.jsx
+++ src/components/facturacion/FacturaForm.jsx
@@ -377,8 +377,28 @@ const FacturaForm = ({ backendStatus }) => {
                     </div>
                 )}
                 {respuesta && (
-                    <div className="mt-8 p-4 border-l-4  w-full ml-8 border-black">
-                        <h3 className="text-black text-xl font-bold mb-2">Resultado</h3>
+                    <div className="mt-8 p-4 border-l-4  w-full ml-8 border-black ">
+                        <div className='w-full flex flex-row justify-between pr-4 pb-6 items-center'>
+
+                            <h3 className="text-black text-xl font-bold ">Resultados</h3>
+                            <button
+                                onClick
+                                className="bg-gray-300 gap-2 text-black rounded-full flex items-center py-4 pl-3 pr-4 h-10"
+
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6">
+                                    <path fill-rule="evenodd" d="M17.663 3.118c.225.015.45.032.673.05C19.876 3.298 21 4.604 21 6.109v9.642a3 3 0 0 1-3 3V16.5c0-5.922-4.576-10.775-10.384-11.217.324-1.132 1.3-2.01 2.548-2.114.224-.019.448-.036.673-.051A3 3 0 0 1 13.5 1.5H15a3 3 0 0 1 2.663 1.618ZM12 4.5A1.5 1.5 0 0 1 13.5 3H15a1.5 1.5 0 0 1 1.5 1.5H12Z" clip-rule="evenodd" />
+                                    <path d="M3 8.625c0-1.036.84-1.875 1.875-1.875h.375A3.75 3.75 0 0 1 9 10.5v1.875c0 1.036.84 1.875 1.875 1.875h1.875A3.75 3.75 0 0 1 16.5 18v2.625c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625v-12Z" />
+                                    <path d="M10.5 10.5a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963 5.23 5.23 0 0 0-3.434-1.279h-1.875a.375.375 0 0 1-.375-.375V10.5Z" />
+                                </svg>
+
+
+                                <p>
+
+                                    Copiar
+                                </p>
+                            </button>
+                        </div>
                         {Array.isArray(respuesta) ? (
                             respuesta.map((resp, index) => (
                                 <div key={index} className="space-y-1 text-gray-700 text-sm">
