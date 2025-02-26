de18f8b011f3d9f6a320ce2858e5bc444663b6cc
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-25 23:16:48 -0300

    boton individual

--- src/components/facturacion/FacturaForm.jsx
+++ src/components/facturacion/FacturaForm.jsx
@@ -9,6 +9,7 @@ const FacturaForm = ({ backendStatus }) => {
     const [tokenStatus, setTokenStatus] = useState(null);
     const [isLoadingToken, setIsLoadingToken] = useState(false);
     const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
+    const [showIndividualForm, setShowIndividualForm] = useState(false);
     const [formData, setFormData] = useState({
         // cuit: '',
         // puntoVenta: '',
@@ -224,36 +225,63 @@ const FacturaForm = ({ backendStatus }) => {
         ));
     };
 
+    const toggleIndividualForm = () => {
+        setShowIndividualForm(!showIndividualForm);
+    };
+
     return (
         <>
             <style>{`select:invalid { color: #9CA3AF; }`}</style>
             <div className="font-coolvetica flex flex-col items-center justify-center w-full bg-gray-50">
-                <div className="py-6 my-auto">
-                    <div className="flex flex-row justify-center items-center gap-2">
-                        {tokenStatus?.valid ? (
-                            <span className="relative flex h-3 w-3">
-                                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-50"></span>
-                                <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-                            </span>
-                        ) : (
-                            <span className="relative flex h-3 w-3">
-                                <span className="relative inline-flex rounded-full h-3 w-3 bg-gray-500"></span>
-                            </span>
-                        )}
-                        <h2 className="text-3xl font-bold text-black text-center">
-                            {tokenStatus?.valid ? 'Conectado' : (
-                                <button
-                                    onClick={handleGenerateToken}
-                                    disabled={isLoadingToken}
-                                    className="text-5xl font-bold text-black text-center"
-                                >
-                                    {isLoadingToken ? 'Conectando' : 'Conectar'}
-                                </button>
+                <div className="py-6 flex flex-row justify-between px-4 items-center">
+
+                    {/* conectado */}
+                    <div className='flex flex-col pr-4'>
+                        <div className="flex flex-row items-center gap-2">
+                            {tokenStatus?.valid ? (
+                                <span className="relative flex h-3 w-3">
+                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-50"></span>
+                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                                </span>
+                            ) : (
+                                <span className="relative flex h-3 w-3">
+                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-gray-500"></span>
+                                </span>
                             )}
-                        </h2>
+                            <h2 className="text-3xl font-bold text-black ">
+                                {tokenStatus?.valid ? 'Conectado' : (
+                                    <button
+                                        onClick={handleGenerateToken}
+                                        disabled={isLoadingToken}
+                                        className="text-5xl font-bold text-black "
+                                    >
+                                        {isLoadingToken ? 'Conectando' : 'Conectar'}
+                                    </button>
+                                )}
+                            </h2>
+                        </div>
+                        <p className="text-gray-400  text-xs ">{backendStatus}.</p>
                     </div>
-                    <p className="text-gray-400 text-center text-xs">{backendStatus}.</p>
+
+
+                    {/* individual */}
+                    <button
+                        onClick={toggleIndividualForm}
+                        className="bg-gray-300 gap-2 text-black rounded-full flex items-center py-4 pl-3 pr-4 h-10"
+
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6">
+                            <path fill-rule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z" clip-rule="evenodd" />
+                            <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+                        </svg>
+
+                        <p>
+
+                            {showIndividualForm ? 'Ocultar' : ' Individual'}
+                        </p>
+                    </button>
                 </div>
+
 
                 <SalesCards ventas={ventasSinFacturar} onToggleFacturar={handleToggleFacturar} />
 
@@ -272,74 +300,78 @@ const FacturaForm = ({ backendStatus }) => {
                     </button>
                 </div>
 
-                <form onSubmit={handleSubmitSingle} className="px-4 mt-8 w-full max-w-md">
-                    <select
-                        name="tipoFactura"
-                        value={formData.tipoFactura}
-                        onChange={handleChange}
-                        className="w-full text-black bg-transparent text-xs border-gray-300 h-10 px-4 rounded-t-3xl border-x border-t border-black transition-all appearance-none"
-                        required
-                    >
-                        <option value="" disabled selected>Tipo de Factura</option>
-                        <option value="A">Factura A</option>
-                        <option value="B">Factura B</option>
-                        <option value="C">Factura C</option>
-                    </select>
-                    <input
-                        type="text"
-                        name="cuit"
-                        value={formData.cuit}
-                        onChange={handleChange}
-                        className="w-full text-black bg-transparent text-xs border-gray-300 h-10 px-4 border-x border-t transition-all"
-                        readOnly
-                        placeholder="CUIT Emisor"
-                    />
-                    <input
-                        type="number"
-                        name="puntoVenta"
-                        value={formData.puntoVenta}
-                        onChange={handleChange}
-                        className="w-full text-black text-xs bg-transparent h-10 border-gray-300 px-4 border-x border-t transition-all"
-                        required
-                        placeholder="Punto de Venta"
-                    />
-                    <input
-                        type="number"
-                        name="importeTrib"
-                        value={formData.importeTrib}
-                        onChange={handleChange}
-                        className="w-full text-black h-10 px-4 bg-transparent text-xs border-x border-t border-gray-300 transition-all"
-                        step="0.01"
-                        min="0"
-                        placeholder="Tasa Municipal"
-                    />
-                    <input
-                        type="number"
-                        name="importeTotal"
-                        value={formData.importeTotal}
-                        onChange={handleChange}
-                        className="w-full border-gray-300 bg-transparent text-black text-xs border-x border-t h-10 px-4 transition-all"
-                        step="0.01"
-                        min="0"
-                        required
-                        placeholder="Importe Total"
-                    />
-                    <input
-                        type="number"
-                        name="importeNeto"
-                        value={formData.importeNeto}
-                        className="w-full border-gray-300 bg-transparent text-black text-xs border-x border-b border-t h-10 px-4 rounded-b-3xl"
-                        readOnly
-                        placeholder="Importe Neto"
-                    />
-                    <button
-                        type="submit"
-                        disabled={!tokenStatus?.valid}
-                        className="w-full bg-black h-20 mb-8 mt-4 rounded-3xl"
-                    >
-                        <p className="text-gray-100 font-bold text-3xl">Factura individual</p>
-                    </button>
-                </form>
+
+
+                {showIndividualForm && (
+                    <form onSubmit={handleSubmitSingle} className="px-4 mt-8 w-full max-w-md">
+                        <select
+                            name="tipoFactura"
+                            value={formData.tipoFactura}
+                            onChange={handleChange}
+                            className="w-full text-black bg-transparent text-xs border-gray-300 h-10 px-4 rounded-t-3xl border-x border-t border-black transition-all appearance-none"
+                            required
+                        >
+                            <option value="" disabled selected>Tipo de Factura</option>
+                            <option value="A">Factura A</option>
+                            <option value="B">Factura B</option>
+                            <option value="C">Factura C</option>
+                        </select>
+                        <input
+                            type="text"
+                            name="cuit"
+                            value={formData.cuit}
+                            onChange={handleChange}
+                            className="w-full text-black bg-transparent text-xs border-gray-300 h-10 px-4 border-x border-t transition-all"
+                            readOnly
+                            placeholder="CUIT Emisor"
+                        />
+                        <input
+                            type="number"
+                            name="puntoVenta"
+                            value={formData.puntoVenta}
+                            onChange={handleChange}
+                            className="w-full text-black text-xs bg-transparent h-10 border-gray-300 px-4 border-x border-t transition-all"
+                            required
+                            placeholder="Punto de Venta"
+                        />
+                        <input
+                            type="number"
+                            name="importeTrib"
+                            value={formData.importeTrib}
+                            onChange={handleChange}
+                            className="w-full text-black h-10 px-4 bg-transparent text-xs border-x border-t border-gray-300 transition-all"
+                            step="0.01"
+                            min="0"
+                            placeholder="Tasa Municipal"
+                        />
+                        <input
+                            type="number"
+                            name="importeTotal"
+                            value={formData.importeTotal}
+                            onChange={handleChange}
+                            className="w-full border-gray-300 bg-transparent text-black text-xs border-x border-t h-10 px-4 transition-all"
+                            step="0.01"
+                            min="0"
+                            required
+                            placeholder="Importe Total"
+                        />
+                        <input
+                            type="number"
+                            name="importeNeto"
+                            value={formData.importeNeto}
+                            className="w-full border-gray-300 bg-transparent text-black text-xs border-x border-b border-t h-10 px-4 rounded-b-3xl"
+                            readOnly
+                            placeholder="Importe Neto"
+                        />
+                        <button
+                            type="submit"
+                            disabled={!tokenStatus?.valid}
+                            className="w-full bg-black h-20 mb-8 mt-4 rounded-3xl"
+                        >
+                            <p className="text-gray-100 font-bold text-3xl">Factura individual</p>
+                        </button>
+                    </form>
+                )}
 
                 {error && (
                     <div className="mt-6 rounded-lg p-4 border-l-4 border-red-500/50">
