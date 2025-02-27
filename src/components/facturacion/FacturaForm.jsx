0dcd27d50030ff84e1fd0ee9c90fcc7169c435ea
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-26 22:52:55 -0300

    fdsa

--- src/components/facturacion/FacturaForm.jsx
+++ src/components/facturacion/FacturaForm.jsx
@@ -310,7 +310,7 @@ const FacturaForm = () => {
                     {/* individual */}
                     <button
                         onClick={toggleIndividualForm}
-                        className="bg-gray-300 gap-2 text-black rounded-full flex items-center py-4 pl-3 pr-4 h-10"
+                        className="bg-gray-300 gap-2 text-black font-bold rounded-full flex items-center py-4 pl-3 pr-4 h-10"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6">
                             <path fillRule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z" clipRule="evenodd" />
