a8997c630fa6f1e6ee6a1763cc41b6589ca1fdaf
Tobías Azcura <tobiascommonplace@gmail.com>
2025-03-09 02:12:38 -0300

    gfds

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -16,14 +16,23 @@ export const Landing: React.FC = () => {
 			<p className="px-4 text-3xl py-6">
 				Productos 
 			</p>
-			<div className="bg-gray-300 mx-4 rounded-3xl h-40"> 
+			<div className="bg-gray-300 mx-4 flex items-center rounded-3xl py-4"> 
 				
-				<p className="px-4 text-xl py-4">
+				<div className="px-4   gap-4 flex justify-between">
+					<div className="flex  flex-col just">
+				<p className=" text-xl text-left ">
 				Facturacion automatica
 				</p>
-				<p className=" px-4">
+				<p className="text-xs pt-2 ">
 					Altamente personalizable. Lo mas simple, podes que cada vez que entre una venta se genere la factura.
 				</p>
+					</div>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-36">
+  <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clip-rule="evenodd" />
+  <path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+</svg>
+
+				</div>
 				</div>
 		</div>
 		
