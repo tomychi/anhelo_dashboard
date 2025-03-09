e5c77779facd247546951cf8d85184c785f54e89
Tobías Azcura <tobiascommonplace@gmail.com>
2025-03-09 16:24:20 -0300

    fdas

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -285,10 +285,9 @@ export const Landing: React.FC = () => {
       </div>
 
       <p className="px-8 text-center text-xs pt-4 pb-8 text-black">
-        Estos productos utilizan actualmente nuestros clientes pero también en
-        base a conversaciones crearemos las soluciones tecnológicas que tu
-        empresa necesite. <br />
-        Gran parte de nuestros productos nacieron así.
+        Estos productos utilizan actualmente nuestros clientes pero también
+        crearemos las soluciones tecnológicas que tu empresa necesite. <br />
+        Todos nuestros productos nacieron así.
       </p>
 
       {/* Empresas que trabajan con nosotros */}
