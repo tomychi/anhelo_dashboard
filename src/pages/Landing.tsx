113423e2ac95219494a8b42187bd63866f9b5e50
Tobías Azcura <tobiascommonplace@gmail.com>
2025-03-09 03:04:43 -0300

    nkl

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,7 +2,7 @@ const products = [
   {
     title: "Facturación automática",
     description:
-      "Altamente personalizable. Olvida gastar horas generando facturas<br /> En su versión con menos intervención humana, cada vez que entre una venta se genera la factura. .",
+      "Altamente personalizable. Olvida gastar horas generando facturas.<br /> En su versión con menos intervención humana, cada vez que entre una venta se genera la factura. .",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -45,7 +45,7 @@ const products = [
   {
     title: "Operaciones",
     description:
-      "Automatizamos la logica de tus operaciones para correrla de la forma mas eficiente posible. <br/> No hay forma de que gastes menos en ejecutarlas.",
+      "Automatizamos la logica de tus operaciones para correrla de la forma mas eficiente y eficaz. <br/> El mejor resultado posible, con el gasto minimo.",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -56,6 +56,24 @@ const products = [
         <path
           fill-rule="evenodd"
           d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "Empleados",
+    description: "Control de presencismo, automatizacion de pago de salarios.",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="h-20 text-black"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
           clip-rule="evenodd"
         />
       </svg>
