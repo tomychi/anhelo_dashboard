8685dfe45bac8c8ea6e996204deb3d183b294cf9
Tobías Azcura <tobiascommonplace@gmail.com>
2025-03-13 01:23:03 -0300

    fdsa

--- src/components/dashboard/KPILineChart.tsx
+++ src/components/dashboard/KPILineChart.tsx
@@ -341,8 +341,8 @@ const KPILineChart = ({ orders }) => {
   // Si no hay KPIs disponibles, mostrar un mensaje
   if (availableKPIs.length === 0) {
     return (
-      <div className="bg-gray-100 mt-4 pt-4 rounded-lg shadow-2xl shadow-gray-400 mb-4 pb-2 h-[250px] flex items-center justify-center">
-        <p className="text-gray-500">
+      <div className="bg-white mt-4 pt-4 rounded-lg shadow-2xl shadow-gray-400 mb-4 pb-2 h-[250px] flex items-center justify-center">
+        <p className="text-gray-500 px-8 text-center">
           No hay gráficos disponibles con tus permisos actuales.
         </p>
       </div>
