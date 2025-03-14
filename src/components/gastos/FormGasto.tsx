481b45760258919d1588311136d909849c2c7767
Tobías Azcura <tobiascommonplace@gmail.com>
2025-03-14 02:38:25 -0300

    gfsd

--- src/components/gastos/FormGasto.tsx
+++ src/components/gastos/FormGasto.tsx
@@ -387,10 +387,10 @@ export const FormGasto = ({ onSuccess }) => {
             <>
               {!isMarketingUser && (
                 <div className="section w-full relative mb-4 z-0">
-                  <p className="text-xs mb-2 font-light">
-                    Selecciona categoría
+                  <p className="text-xs mb-2 font-light text-center">
+                    Categoría
                   </p>
-                  <div className="flex flex-wrap gap-2">
+                  <div className="flex flex-wrap justify-center gap-2">
                     {CATEGORIAS.map((category) => (
                       <div
                         key={category}
@@ -406,6 +406,19 @@ export const FormGasto = ({ onSuccess }) => {
                             : "bg-gray-200 text-black"
                         }`}
                       >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          class="h-4 mr-2"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M4.5 2A2.5 2.5 0 0 0 2 4.5v3.879a2.5 2.5 0 0 0 .732 1.767l7.5 7.5a2.5 2.5 0 0 0 3.536 0l3.878-3.878a2.5 2.5 0 0 0 0-3.536l-7.5-7.5A2.5 2.5 0 0 0 8.38 2H4.5ZM5 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+
                         {category}
                       </div>
                     ))}
