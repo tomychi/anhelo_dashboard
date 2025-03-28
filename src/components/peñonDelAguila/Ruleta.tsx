03e6c765d25e26933311fbfc9734d413d0e9843d
Tobías Azcura <tobiascommonplace@gmail.com>
2025-03-28 13:48:05 -0300

    hdgf

--- src/components/peñonDelAguila/Ruleta.tsx
+++ src/components/peñonDelAguila/Ruleta.tsx
@@ -3,6 +3,7 @@ import RuletaModal from "./RuletaModal";
 import ConfiguracionRuletaModal from "./ConfiguracionRuletaModal";
 import logo from "../../assets/AGUILA BLANCO-1.png";
 import arrowIcon from "../../assets/arrowIcon.png";
+import LoadingPoints from "../LoadingPoints";
 
 // Función para generar productos a partir de los números seleccionados
 const generateProducts = (selectedNumbers) => {
@@ -229,20 +230,25 @@ export const Ruleta = () => {
               Configurar
             </button>
             <button
-              className="h-10 flex flex-row items-center bg-gray-200 rounded-full px-4 font-bold font-coolvetica gap-2 text-xl"
+              className="h-10 items-center bg-gray-200 rounded-full px-4 font-bold font-coolvetica "
               onClick={spinWheel}
               disabled={spinning || products.length === 0}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="h-6"
-              >
-                <path d="M9.375 3a1.875 1.875 0 0 0 0 3.75h1.875v4.5H3.375A1.875 1.875 0 0 1 1.5 9.375v-.75c0-1.036.84-1.875 1.875-1.875h3.193A3.375 3.375 0 0 1 12 2.753a3.375 3.375 0 0 1 5.432 3.997h3.943c1.035 0 1.875.84 1.875 1.875v.75c0 1.036-.84 1.875-1.875 1.875H12.75v-4.5h1.875a1.875 1.875 0 1 0-1.875-1.875V6.75h-1.5V4.875C11.25 3.839 10.41 3 9.375 3ZM11.25 12.75H3v6.75a2.25 2.25 0 0 0 2.25 2.25h6v-9ZM12.75 12.75v9h6.75a2.25 2.25 0 0 0 2.25-2.25v-6.75h-9Z" />
-              </svg>
-
-              {spinning ? "Girando..." : "Sortear"}
+              {spinning ? (
+                <LoadingPoints />
+              ) : (
+                <div className="flex flex-row gap-2 text-xl">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-6"
+                  >
+                    <path d="M9.375 3a1.875 1.875 0 0 0 0 3.75h1.875v4.5H3.375A1.875 1.875 0 0 1 1.5 9.375v-.75c0-1.036.84-1.875 1.875-1.875h3.193A3.375 3.375 0 0 1 12 2.753a3.375 3.375 0 0 1 5.432 3.997h3.943c1.035 0 1.875.84 1.875 1.875v.75c0 1.036-.84 1.875-1.875 1.875H12.75v-4.5h1.875a1.875 1.875 0 1 0-1.875-1.875V6.75h-1.5V4.875C11.25 3.839 10.41 3 9.375 3ZM11.25 12.75H3v6.75a2.25 2.25 0 0 0 2.25 2.25h6v-9ZM12.75 12.75v9h6.75a2.25 2.25 0 0 0 2.25-2.25v-6.75h-9Z" />
+                  </svg>
+                  <p>Sortear</p>
+                </div>
+              )}
             </button>
           </div>
         </div>
