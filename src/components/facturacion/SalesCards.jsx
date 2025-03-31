1cb154ee94d4e8122e5491129d38734156f7b84d
Tobías Azcura <tobiascommonplace@gmail.com>
2025-03-30 21:23:42 -0300

    gfds

--- src/components/facturacion/SalesCards.jsx
+++ src/components/facturacion/SalesCards.jsx
@@ -8,24 +8,28 @@ const SalesCards = ({ ventas, onToggleFacturar }) => {
   );
 
   return (
-    <div className="w-full  font-coolvetica">
+    <div className="w-full font-coolvetica">
       {/* title */}
-      <div className="flex px-4 flex-row items-baseline  mb-2">
-        <h2 className="font-bold  text-xs">Total a facturar:</h2>
-        <p className=" font-bold ml-2">{currencyFormat(totalVentas)}</p>
+      <div className="flex px-4 flex-row items-baseline mb-2">
+        <h2 className="font-bold text-xs">Total a facturar:</h2>
+        <p className="font-bold ml-2">{currencyFormat(totalVentas)}</p>
       </div>
       {/* cards */}
       <div className="flex flex-row px-4 gap-2 overflow-x-auto">
         {ventas.map((venta) => (
           <button
             key={venta.id}
-            onClick={() => onToggleFacturar(venta.id)}
-            className={` w-20 border border-gray-200  flex-shrink-0 rounded-xl 
-                            flex flex-col items-center cursor-pointer transition-colors
-                            `}
+            onClick={(e) => {
+              // Detener la propagación del evento para evitar que llegue al formulario
+              e.stopPropagation();
+              e.preventDefault();
+              onToggleFacturar(venta.id);
+            }}
+            className={`w-20 border border-gray-200 flex-shrink-0 rounded-xl 
+                      flex flex-col items-center cursor-pointer transition-colors`}
           >
             <div
-              className={`text-center flex flex-row gap-0.5 w-full bg-gray-200 items-center flex justify-center ${venta.quiereFacturarla ? "text-green-500" : "text-red-500"}  rounded-t-xl text-xs py-1 font-bold`}
+              className={`text-center flex flex-row gap-0.5 w-full bg-gray-200 items-center flex justify-center ${venta.quiereFacturarla ? "text-green-500" : "text-red-500"} rounded-t-xl text-xs py-1 font-bold`}
             >
               {venta.quiereFacturarla ? (
                 <svg
@@ -35,9 +39,9 @@ const SalesCards = ({ ventas, onToggleFacturar }) => {
                   className="h-3"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               ) : (
@@ -48,9 +52,9 @@ const SalesCards = ({ ventas, onToggleFacturar }) => {
                   className="h-3"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               )}
