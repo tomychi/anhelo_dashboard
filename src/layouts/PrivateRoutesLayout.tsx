abbbb4ee0560a800d6dcedaa5f11bde53cce4702
Tobías Azcura <tobiascommonplace@gmail.com>
2025-03-31 14:03:54 -0300

    denegado

--- src/layouts/PrivateRoutesLayout.tsx
+++ src/layouts/PrivateRoutesLayout.tsx
@@ -7,6 +7,7 @@ import {
   hasPermissionForRoute,
 } from "../utils/permissionsUtils";
 import { RootState } from "../redux/configureStore";
+import arrowIcon from "../assets/arrowIcon.png";
 
 interface PrivateRoutesLayoutProps {
   children: ReactNode;
@@ -37,31 +38,39 @@ const PrivateRoutesLayout: React.FC<PrivateRoutesLayoutProps> = ({
     // Mostrar mensaje de error de permisos
     return (
       <div className="flex flex-col font-coolvetica items-center justify-center h-[80vh]">
-        <div className=" p-8  text-center max-w-md">
+        <div className=" p-8 flex justify-center items-center flex-col text-center max-w-md">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-16 w-16 text-red-main mx-auto mb-4"
-            fill="none"
             viewBox="0 0 24 24"
-            stroke="currentColor"
+            fill="currentColor"
+            className="h-20 text-red-main"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              fill-rule="evenodd"
+              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+              clip-rule="evenodd"
             />
           </svg>
+
           <h1 className="text-2xl font-bold text-red-main mb-2">
             Acceso denegado
           </h1>
-          <p className="text-gray-600 mb-4">
-            No tienes permisos para acceder a esta sección.
+          <p className="font-light text-xs mb-4">
+            No tienes permisos para acceder a este feature.
           </p>
           <button
             onClick={() => window.history.back()}
-            className="bg-red-main text-white px-4 h-10 rounded-full hover:bg-red-700 transition-colors"
+            className="bg-gray-200 text-red-main font-bold px-4 h-10 rounded-full   flex flex-row items-center mt-6 w-fit px-4 gap-2"
           >
+            <img
+              src={arrowIcon}
+              className="h-2 transform rotate-180"
+              alt="Volver"
+              style={{
+                filter:
+                  "invert(20%) sepia(100%) saturate(5000%) hue-rotate(0deg) brightness(90%) contrast(150%)",
+              }}
+            />
             Volver
           </button>
         </div>
