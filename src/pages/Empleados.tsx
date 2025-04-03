4d2e6900f573cd8b50ae4b1cf577a799a93084b5
Tobías Azcura <tobiascommonplace@gmail.com>
2025-04-03 20:50:53 -0300

    gfsd

--- src/pages/Empleados.tsx
+++ src/pages/Empleados.tsx
@@ -454,7 +454,7 @@ export const Empleados = () => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-row justify-between font-coolvetica items-center mt-8 mx-4 pb-8">
-        <p className="text-black font-bold text-4xl mt-1">Empleados</p>
+        <p className="text-black font-bold text-4xl mt-1">Equipo</p>
         <button
           onClick={() => setShowForm(true)}
           className="bg-gray-200 gap-2 text-black rounded-full flex items-center pt-3 pb-4 pl-3 pr-4 h-10"
@@ -466,12 +466,13 @@ export const Empleados = () => {
             className="h-6"
           >
             <path
-              fillRule="evenodd"
-              d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
-              clipRule="evenodd"
+              fill-rule="evenodd"
+              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+              clip-rule="evenodd"
             />
           </svg>
-          <p className="font-bold">Nuevo empleado</p>
+
+          <p className="font-bold">Nuevo miembro</p>
         </button>
       </div>
 
@@ -605,7 +606,6 @@ export const Empleados = () => {
         )}
       </div>
 
-      {/* Modal para crear nuevo empleado */}
       {showForm && (
         <div className="fixed inset-0 z-50 flex items-end font-coolvetica justify-center">
           <div
