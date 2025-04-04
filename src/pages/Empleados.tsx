05b692cbdf18398860d4bbf58a54555f8b67c6e8
Tobías Azcura <tobiascommonplace@gmail.com>
2025-04-03 21:10:09 -0300

    gfds

--- src/pages/Empleados.tsx
+++ src/pages/Empleados.tsx
@@ -439,30 +439,30 @@ export const Empleados = () => {
 
   return (
     <div className="font-coolvetica overflow-hidden flex flex-col items-center justify-center w-full">
-      <div className="py-8 flex flex-col  w-full items-baseline">
-        <div className="flex flex-row justify-between w-full px-4">
-          <h2 className="text-3xl font-bold">Equipo</h2>
-          <button
-            onClick={() => setShowForm(true)}
-            className="bg-gray-200 gap-2 text-black rounded-full flex items-center px-4 h-10 ml-4"
+      <div className="flex flex-row py-8 justify-between w-full px-4">
+        <h2 className="text-3xl font-bold">Equipo</h2>
+        <button
+          onClick={() => setShowForm(true)}
+          className="bg-gray-200 gap-2 text-black rounded-full flex items-center px-4 h-10 ml-4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-6"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <p className="font-bold">Nuevo miembro</p>
-          </button>
-        </div>
+            <path
+              fillRule="evenodd"
+              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <p className="font-bold">Nuevo miembro</p>
+        </button>
+      </div>
 
-        <div className="flex items-center w-full h-10 gap-1 mt-2 rounded-lg border-4 border-black focus:ring-0 font-coolvetica text-black text-xs font-light">
+      <div className="w-full px-4">
+        <div className="flex items-center  w-full h-10 gap-1 mt-2 rounded-lg border-4 border-black focus:ring-0 font-coolvetica text-black text-xs font-light">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
